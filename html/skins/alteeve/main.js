e983f27de7e84b1e2365c1a7fff6d9462b796b22
Digimer <digimer@alteeve.ca>
2017-06-12 17:39:09 -0400

    * Changed scancore-update-states to report link speeds > 100 Gbps as 0. * Added polling to update the network state as needed.
    Signed-off-by: Digimer <digimer@alteeve.ca>

--- html/skins/alteeve/main.js
+++ html/skins/alteeve/main.js
@@ -2,21 +2,23 @@ $(function() {
 	var say_up           = $('#network_link_state').data('up');
 	var say_down         = $('#network_link_state').data('down');
 	var say_speed_suffix = $('#network_link_speed').data('mbps');
-	console.log('say_up: ['+say_up+'], say_down: ['+say_down+'], say_speed_suffix: ['+say_speed_suffix+']');
+	//console.log('say_up: ['+say_up+'], say_down: ['+say_down+'], say_speed_suffix: ['+say_speed_suffix+']');
 	if($("#network_status").length) {
 		//alert('network status exists.');
-		$.getJSON('/status/network.json', { get_param: 'value' }, function(data) {
-			$.each(data.networks, function(index, element) {
-				console.log('entry: ['+index+'], name: ['+element.name+'], mac: ['+element.mac+'], link: ['+element.link+']');
-				var link = say_up;
-				if (element.link == 0) {
-					link = say_down;
-				}
-				$("#"+element.name+"_mac").text(element.mac);
-				$("#"+element.name+"_link").text(link);
-				$("#"+element.name+"_speed").text(element.speed+' '+say_speed_suffix);
-			});
-		});    
+		setInterval(function() {
+			$.getJSON('/status/network.json', { get_param: 'value' }, function(data) {
+				$.each(data.networks, function(index, element) {
+					//console.log('entry: ['+index+'], name: ['+element.name+'], mac: ['+element.mac+'], link: ['+element.link+']');
+					var link = say_up;
+					if (element.link == 0) {
+						link = say_down;
+					}
+					$("#"+element.name+"_mac").text(element.mac);
+					$("#"+element.name+"_link").text(link);
+					$("#"+element.name+"_speed").text(element.speed+' '+say_speed_suffix);
+				});
+			});    
+		}, 1000);
 	}
 	if($("#disk_status").length) {
 		//alert('disk status exists.');
