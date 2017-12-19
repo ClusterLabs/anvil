98edf6887c02d24d951b3394ec3b01e57ec8851a
Chris Johnson <christopher.johnson@senecacollege.ca>
2017-12-19 11:47:42 -0500

    Fixed typo in DeviceInteractions Tools declaration.

--- Anvil/Tools.pm
+++ Anvil/Tools.pm
@@ -46,7 +46,7 @@ use Anvil::Tools::System;
 use Anvil::Tools::Template;
 use Anvil::Tools::Words;
 use Anvil::Tools::Validate;
-use Anvil::Tools:DeviceInteractions;
+use Anvil::Tools::DeviceInteractions;
 
 =pod
 
