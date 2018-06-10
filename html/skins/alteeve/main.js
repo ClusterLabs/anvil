86259a4783f5848cf7424ad4aeaa6bfde5cb3ce2
Digimer <digimer@alteeve.ca>
2018-06-10 00:27:08 -0400

    * Got the network.xml and network.json files fixed and parsable. * Moved config JS to config.js.
    Signed-off-by: Digimer <digimer@alteeve.ca>

--- html/skins/alteeve/main.js
+++ html/skins/alteeve/main.js
@@ -2,43 +2,8 @@ $.ajaxSetup({
     cache: false
 });
 $(function() { 
-	var say_up           = $('#network_link_state').data('up');
-	var say_down         = $('#network_link_state').data('down');
-	var say_speed_suffix = $('#network_link_speed').data('mbps');
-	//console.log('say_up: ['+say_up+'], say_down: ['+say_down+'], say_speed_suffix: ['+say_speed_suffix+']');
-	if($("#network_status").length) {
-		//alert('network status exists.');
-		setInterval(function() {
-			$.getJSON('/status/network.json', { get_param: 'value' }, function(data) {
-				$.each(data.networks, function(index, element) {
-					
-					console.log('entry: ['+index+'], name: ['+element.name+'], mac: ['+element.mac+'], link: ['+element.link+'], up order: ['+element.order+']');
-					
-					var link = say_up;
-					if (element.link == 0) {
-						link = say_down;
-					}
-					$("#"+element.name+"_mac").text(element.mac);
-					$("#"+element.name+"_link").text(link);
-					$("#"+element.name+"_speed").text(element.speed+' '+say_speed_suffix);
-					$("#"+element.name+"_order").text(element.order);
-				});
-			});
-		}, 1000);
-	}
-	else
-	{
-		//alert('network status strings not loaded.');
-	}
-	if($("#disk_status").length) {
-		//alert('disk status exists.');
-		//$("#bar").text('B');
-	}
 });
 
 $( window ).on( "load", function()
 {
-	// NOTE: Disabled for now. Clears the URL to remove everything off after '?'.
-	//var newURL = location.href.split("?")[0];
-	//window.history.pushState('object', document.title, newURL);
 })
