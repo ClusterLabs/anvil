7ad89d7ec20e9fb7e0ce6b62e167477f8c1a43e8
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2021-02-17 23:12:47 -0500

    feat(striker-ui): add default theme

---
+++ striker-ui/lib/consts/DEFAULT_THEME.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+const DEFAULT_THEME: Readonly<DefaultTheme> = {
+  colors: {
+    primary: '#f2f2f2',
+    secondary: '#343434',
+  },
+};
+
+export default DEFAULT_THEME;
