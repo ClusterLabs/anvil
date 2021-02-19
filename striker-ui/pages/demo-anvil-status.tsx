91de8ca585c521e46741c5f4ad2c7581cf3d94ac
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2021-02-19 01:03:32 -0500

    fix(striker-ui): fetch Anvil status with interval

--- striker-ui/pages/demo-anvil-status.tsx
+++ striker-ui/pages/demo-anvil-status.tsx
@@ -1,23 +1,13 @@
-import { GetServerSidePropsResult, InferGetServerSidePropsType } from 'next';
+import { NextPage } from 'next';
+import useOneAnvil from '../lib/anvil/useOneAnvil';
 
-import API_BASE_URL from '../lib/consts/API_BASE_URL';
+const DemoAnvilStatus: NextPage = (): JSX.Element => {
+  const {
+    anvilStatus: { nodes, timestamp },
+    error,
+    isLoading,
+  } = useOneAnvil(`d61c0383-5d82-4d9f-a193-b4a31cff1ceb`);
 
-import fetchJSON from '../lib/fetchers/fetchJSON';
-
-export async function getServerSideProps(): Promise<
-  GetServerSidePropsResult<AnvilStatus>
-> {
-  return {
-    props: await fetchJSON(
-      `${API_BASE_URL}/api/anvils/1aded871-fcb1-4473-9b97-6e9c246fc568`,
-    ),
-  };
-}
-
-function DemoAnvilStatus({
-  nodes,
-  timestamp,
-}: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
   return (
     <div>
       <h1>Demo Anvil Status</h1>
@@ -25,8 +15,12 @@ function DemoAnvilStatus({
       <pre>{JSON.stringify(nodes, null, 4)}</pre>
       <h2>timestamp</h2>
       <pre>{timestamp}</pre>
+      <h2>isLoading</h2>
+      <pre>{isLoading}</pre>
+      <h2>error</h2>
+      <pre>{error?.message}</pre>
     </div>
   );
-}
+};
 
 export default DemoAnvilStatus;
