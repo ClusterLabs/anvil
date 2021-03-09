1e32b3d32f16edcdca9c44523b109ca53a43551d
Josue <josue.quilon-barrios@senecacollege.ca>
2021-03-09 11:30:01 -0500

    refactor: remove server side rendering for now

--- striker-ui/pages/index.tsx
+++ striker-ui/pages/index.tsx
@@ -1,4 +1,3 @@
-import { GetServerSidePropsResult } from 'next';
 import { Grid } from '@material-ui/core';
 
 import Anvils from '../components/Anvils';
@@ -6,36 +5,23 @@ import Nodes from '../components/Nodes';
 import CPU from '../components/CPU';
 import SharedStorage from '../components/SharedStorage';
 import ReplicatedStorage from '../components/ReplicatedStorage';
-import State from '../components/State';
 import Memory from '../components/Memory';
 
-import API_BASE_URL from '../lib/consts/API_BASE_URL';
-import fetchJSON from '../lib/fetchers/fetchJSON';
-
-import 'typeface-muli';
-
-export async function getServerSideProps(): Promise<
-  GetServerSidePropsResult<AnvilList>
-> {
-  return {
-    props: await fetchJSON(`${API_BASE_URL}/api/anvils`),
-  };
-}
+import 'fontsource-roboto';
 
 const Home = (): JSX.Element => {
   return (
     <Grid container alignItems="center" justify="space-around">
       <Grid item xs={3}>
         <Anvils />
-        <State />
         <Nodes />
       </Grid>
       <Grid item xs={5}>
         <ReplicatedStorage />
       </Grid>
       <Grid item xs={3}>
-        <CPU />
         <SharedStorage />
+        <CPU />
         <Memory />
       </Grid>
     </Grid>
