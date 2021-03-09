f45c0cd9568f5be58e6dd833898891d45607ff3a
Josue <josue.quilon-barrios@senecacollege.ca>
2021-03-09 11:21:37 -0500

    refactor: elements related to previous approach (before material ui)

--- striker-ui/pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-function hello(req: NextApiRequest, res: NextApiResponse): void {
-  res.status(200).json({ name: 'John Doe' });
-}
-
-export default hello;
