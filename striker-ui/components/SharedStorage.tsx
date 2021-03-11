ce56ad092be5090968cb2fa274df74e42e3b107a
Josue <josue.quilon-barrios@senecacollege.ca>
2021-03-11 15:46:42 -0500

    feat: add temaplate using new components to SharedStorage

--- striker-ui/components/SharedStorage.tsx
+++ striker-ui/components/SharedStorage.tsx
@@ -1,10 +1,22 @@
+import { Grid } from '@material-ui/core';
+import InnerPanel from './InnerPanel';
 import Panel from './Panel';
-import Text from './Text/HeaderText';
+import { HeaderText, BodyText } from './Text';
 
 const SharedStorage = (): JSX.Element => {
   return (
     <Panel>
-      <Text text="Shared Storage" />
+      <Grid container alignItems="center" justify="space-around">
+        <Grid item xs={12}>
+          <HeaderText text="Shared Storage" />
+        </Grid>
+        <Grid item xs={12}>
+          <BodyText text="Mount /mnt/shared" />
+        </Grid>
+        <Grid item xs={12}>
+          <InnerPanel />
+        </Grid>
+      </Grid>
     </Panel>
   );
 };
