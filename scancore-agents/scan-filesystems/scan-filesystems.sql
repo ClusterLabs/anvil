53d654fd9dc4749a37a81050d1931127171b3f83
Digimer <digimer@alteeve.ca>
2021-03-19 18:19:29 -0400

    * Got scan-filesystem to the point where it now collects the data it needs. No processing done yet though. Also reworked the SQL schema for the agent to record more data. * Created Storage->parse_df that, shock!, parses 'df' output. Finished the long-ago started ->parse_lsblk as well.
    Signed-off-by: Digimer <digimer@alteeve.ca>

--- scancore-agents/scan-filesystems/scan-filesystems.sql
+++ scancore-agents/scan-filesystems/scan-filesystems.sql
@@ -6,16 +6,16 @@ CREATE TABLE scan_filesystems (
     scan_filesystem_uuid             uuid                        primary key,    -- This comes from the file system's UUID 
     scan_filesystem_host_uuid        uuid                        not null,       -- The host that the file system is mounted on. Note that some FSes, like those from USB, can move between hosts.
     scan_filesystem_type             text                        not null,       -- This is the name of the file system type.  
-    scan_filesystem_device_path      text                        not null,       -- This is the backing device of the file system.
-    scan_filesystem_mount_path       text                        not null,       -- This is the name of the mount point.  
-    scan_filesystem_media_type       text                        not null,       -- Optional description of the drive's media tpe (usb, nvme, sata_ssd, sata_platter, md, raid, optical, sdcard, etc - 'unknown')
+    scan_filesystem_kernel_name      text                        not null,       -- This is the backing device of the file system.
+    scan_filesystem_mount_point      text                        not null,       -- This is the name of the mount point.  
+    scan_filesystem_transport        text                        not null,       -- Optional description of the drive's transport (usb, nvme, sata, sata, md, raid, optical, sdcard, etc - 'unknown')
+    scan_filesystem_media_type       text                        not null,       -- This is set to 'ssd' for solid state, 'platter' for spinning rust, 'network' for network mounts, etc.
     scan_filesystem_vendor           text                        not null,       -- Optional vendor of the drive the partition is on
     scan_filesystem_model            text                        not null,       -- Optional model of the drive the partiton is on
     scan_filesystem_serial_number    text                        not null,       -- Optional serial number of the drive the partition is on
-    scan_filesystem_removable        text                        not null,       -- Optional string indicating if the file system is on removable media ('yes', 'no', 'unknown')
-    scan_filesystem_description      text                        not null,       -- Free form description of the device. Usually make / model / serial number 
-    scan_filesystem_size             numeric                     not null,       -- The size of the PV in bytes
-    scan_filesystem_free             numeric                     not null,       -- The free space, in bytes.
+    scan_filesystem_description      text                        not null,       -- Free form description of the device.
+    scan_filesystem_size             numeric                     not null,       -- The size of the partition, in bytes
+    scan_filesystem_used             numeric                     not null,       -- The used space, in bytes.
     modified_date                    timestamp with time zone    not null,
     
     FOREIGN KEY(scan_filesystem_host_uuid) REFERENCES hosts(host_uuid)
@@ -27,12 +27,16 @@ CREATE TABLE history.scan_filesystems (
     scan_filesystem_uuid             uuid,
     scan_filesystem_host_uuid        uuid,
     scan_filesystem_type             text,
-    scan_filesystem_device_path      text,
-    scan_filesystem_mount_path       text,
-    scan_filesystem_media            text,
+    scan_filesystem_kernel_name      text,
+    scan_filesystem_mount_point      text,
+    scan_filesystem_transport        text,
+    scan_filesystem_media_type       text,
+    scan_filesystem_vendor           text, 
+    scan_filesystem_model            text, 
+    scan_filesystem_serial_number    text, 
     scan_filesystem_description      text,
     scan_filesystem_size             numeric,
-    scan_filesystem_free             numeric,
+    scan_filesystem_used             numeric,
     modified_date                    timestamp with time zone    not null
 );
 ALTER TABLE history.scan_filesystems OWNER TO admin;
@@ -47,23 +51,31 @@ BEGIN
         (scan_filesystem_uuid,
          scan_filesystem_host_uuid, 
          scan_filesystem_type, 
-         scan_filesystem_device_path, 
-         scan_filesystem_mount_path, 
-         scan_filesystem_media, 
+         scan_filesystem_kernel_name, 
+         scan_filesystem_mount_point, 
+         scan_filesystem_transport, 
+         scan_filesystem_media_type, 
+         scan_filesystem_vendor, 
+         scan_filesystem_model, 
+         scan_filesystem_serial_number, 
          scan_filesystem_description, 
          scan_filesystem_size, 
-         scan_filesystem_free, 
+         scan_filesystem_used, 
          modified_date)
     VALUES
         (history_scan_filesystems.scan_filesystem_uuid,
          history_scan_filesystems.scan_filesystem_host_uuid, 
          history_scan_filesystems.scan_filesystem_type, 
-         history_scan_filesystems.scan_filesystem_device_path, 
-         history_scan_filesystems.scan_filesystem_mount_path, 
-         history_scan_filesystems.scan_filesystem_media, 
+         history_scan_filesystems.scan_filesystem_kernel_name, 
+         history_scan_filesystems.scan_filesystem_mount_point, 
+         history_scan_filesystems.scan_filesystem_transport, 
+         history_scan_filesystems.scan_filesystem_media_type, 
+         history_scan_filesystems.scan_filesystem_vendor, 
+         history_scan_filesystems.scan_filesystem_model, 
+         history_scan_filesystems.scan_filesystem_serial_number, 
          history_scan_filesystems.scan_filesystem_description, 
          history_scan_filesystems.scan_filesystem_size, 
-	 history_scan_filesystems.scan_filesystem_free, 
+	 history_scan_filesystems.scan_filesystem_used, 
          history_scan_filesystems.modified_date);
     RETURN NULL;
 END;
