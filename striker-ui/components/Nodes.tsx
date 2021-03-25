ec8b171bea2a17860b7208037feabb63e78d947b
Josue <josue.quilon-barrios@senecacollege.ca>
2021-03-25 10:54:39 -0400

    refactor: modify Nodes to fetch and display real data

--- striker-ui/components/Nodes.tsx
+++ striker-ui/components/Nodes.tsx
@@ -1,12 +1,20 @@
-import InnerPanel from './InnerPanel';
 import Panel from './Panel';
 import Text from './Text/HeaderText';
+import AnvilNode from './AnvilNode';
+import PeriodicFetch from '../lib/fetchers/periodicFetch';
 
-const Nodes = (): JSX.Element => {
+const Nodes = ({ anvil }: { anvil: AnvilListItem }): JSX.Element => {
+  const { data } = PeriodicFetch<AnvilStatus>(
+    `${process.env.NEXT_PUBLIC_API_URL}/anvils/get_status?anvil_uuid=`,
+    anvil?.anvil_uuid,
+  );
+  /* eslint-disable no-param-reassign */
+  if (anvil) anvil.anvil_status = data;
+
   return (
     <Panel>
       <Text text="Nodes" />
-      <InnerPanel />
+      <AnvilNode node={anvil?.anvil_status} />
     </Panel>
   );
 };
