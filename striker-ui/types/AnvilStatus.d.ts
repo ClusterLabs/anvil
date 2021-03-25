799f6d65aaaf3141ffbfc44d8aba2c814780a02e
Josue <josue.quilon-barrios@senecacollege.ca>
2021-03-25 11:02:50 -0400

    refactor: modify AnvilStatus type to match current definition in docs

--- striker-ui/types/AnvilStatus.d.ts
+++ striker-ui/types/AnvilStatus.d.ts
@@ -1,4 +1,8 @@
 declare type AnvilStatus = {
-  nodes: AnvilNodeStatus[];
-  timestamp: number;
+  anvil_state: 'optimal' | 'not_ready' | 'degraded';
+  nodes: Array<{
+    state: 'unknown' | 'off' | 'on' | 'accessible' | 'ready';
+    state_percent: number;
+    state_message: string;
+  }>;
 };
