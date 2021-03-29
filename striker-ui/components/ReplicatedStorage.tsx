0d7a3651ce4d25981f7e98f90df67a42fa24003a
Josue <josue.quilon-barrios@senecacollege.ca>
2021-03-29 18:41:16 -0400

    fix: remove using old version of InnerPanel

--- striker-ui/components/ReplicatedStorage.tsx
+++ striker-ui/components/ReplicatedStorage.tsx
@@ -1,13 +1,11 @@
 import Panel from './Panel';
 import { HeaderText } from './Text';
-import InnerPanel from './InnerPanel';
-import ProgressBar from './ProgressBar';
+import ProgressBar from './ProgressBarBck';
 
 const ReplicatedStorage = (): JSX.Element => {
   return (
     <Panel>
       <HeaderText text="Replicated Storage" />
-      <InnerPanel />
       <ProgressBar allocated={20} />
     </Panel>
   );
