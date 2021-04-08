2ec90b1ea3cf3cd11f157230b35eb5a76a75a34a
Josue <josue.quilon-barrios@senecacollege.ca>
2021-04-08 10:35:01 -0400

    refactor: group up AnvilStatus types

--- striker-ui/types/AnvilStatus.d.ts
+++ striker-ui/types/AnvilStatus.d.ts
@@ -1,3 +1,9 @@
+declare type AnvilStatusNode = {
+  state: 'unknown' | 'off' | 'on' | 'accessible' | 'ready';
+  state_percent: number;
+  state_message: string;
+};
+
 declare type AnvilStatus = {
   anvil_state: 'optimal' | 'not_ready' | 'degraded';
   nodes: Array<{
--- striker-ui/types/AnvilStatusNode.d.ts
+++
@@ -1,5 +0,0 @@
-declare type AnvilStatusNode = {
-  state: 'unknown' | 'off' | 'on' | 'accessible' | 'ready';
-  state_percent: number;
-  state_message: string;
-};
