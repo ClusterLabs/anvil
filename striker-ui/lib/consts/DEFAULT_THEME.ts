f3b5a8cfbb8f60d48152a8fd55d0fb98f07e6ff5
Josue <josue.quilon-barrios@senecacollege.ca>
2021-04-28 11:57:55 -0400

    style: add new colours to consts

--- striker-ui/lib/consts/DEFAULT_THEME.ts
+++ striker-ui/lib/consts/DEFAULT_THEME.ts
@@ -9,4 +9,5 @@ export const PURPLE_OFF = '#7353BA';
 export const BLUE = '#4785FF';
 export const GREY = '#E5E5E5';
 export const HOVER = '#444';
-export const UNSELECTED = '#888';
+export const UNSELECTED = '#666';
+export const DIVIDER = '#888';
