334623e3bfd315927285831eb0a60f5b529fa677
Josue <josue.quilon-barrios@senecacollege.ca>
2021-04-29 15:48:34 -0400

    refactor: change data type to match the specs in the design document

--- striker-ui/types/AnvilServers.d.ts
+++ striker-ui/types/AnvilServers.d.ts
@@ -1,7 +1,15 @@
 declare type AnvilServer = {
   server_name: string;
   server_uuid: string;
-  server_state: string;
+  server_state:
+    | 'running'
+    | 'idle'
+    | 'paused'
+    | 'in_shutdown'
+    | 'shut_off'
+    | 'crashed'
+    | 'pmsuspended'
+    | 'migrating';
   server_host_index: number;
 };
 
