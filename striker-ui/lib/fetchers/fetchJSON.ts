431b88aa11319d8d311e1dd0ba61a1fca410e5e0
Josue <josue.quilon-barrios@senecacollege.ca>
2021-05-25 11:45:48 -0400

    refactor: rewrite function using es6 for consistency

--- striker-ui/lib/fetchers/fetchJSON.ts
+++ striker-ui/lib/fetchers/fetchJSON.ts
@@ -1,5 +1,5 @@
-function fetchJSON<T>(...args: [RequestInfo, RequestInit?]): Promise<T> {
+const fetchJSON = <T>(...args: [RequestInfo, RequestInit?]): Promise<T> => {
   return fetch(...args).then((response: Response) => response.json());
-}
+};
 
 export default fetchJSON;
