96576f505571080249c60bb17d276c2f2f791128
Josue <josue.quilon-barrios@senecacollege.ca>
2021-06-15 16:23:17 -0400

    refactor: remove react-vnc and add react-vnc-display for vnc connection in the server page

--- striker-ui/components/Display.tsx
+++ striker-ui/components/Display.tsx
@@ -1,10 +1,26 @@
+import { useState, useEffect } from 'react';
+// import { VncScreen } from 'react-vnc';
+import VncDisplay from 'react-vnc-display';
 import { Panel } from './Panels';
 import { HeaderText } from './Text';
 
 const Display = (): JSX.Element => {
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(typeof window !== 'undefined');
+  }, [mounted]);
+
   return (
     <Panel>
       <HeaderText text="Display" />
+      <VncDisplay
+        url="wss://spain.cdot.systems:5000/"
+        style={{
+          width: '51vw',
+          height: '70vh',
+        }}
+      />
     </Panel>
   );
 };
