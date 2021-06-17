e4f617ffc044f61c4d6fdc8555c769b774c161ce
Josue <josue.quilon-barrios@senecacollege.ca>
2021-06-17 10:56:00 -0400

    refactor: remove unnecessary setup for displaying vnc feed

--- striker-ui/components/Display.tsx
+++ striker-ui/components/Display.tsx
@@ -1,23 +1,15 @@
-import { useState, useEffect } from 'react';
-// import { VncScreen } from 'react-vnc';
 import VncDisplay from 'react-vnc-display';
 import { Panel } from './Panels';
 import { HeaderText } from './Text';
 
 const Display = (): JSX.Element => {
-  const [mounted, setMounted] = useState<boolean>(false);
-
-  useEffect(() => {
-    setMounted(typeof window !== 'undefined');
-  }, [mounted]);
-
   return (
     <Panel>
       <HeaderText text="Display" />
       <VncDisplay
         url="wss://spain.cdot.systems:5000/"
         style={{
-          width: '51vw',
+          width: '50vw',
           height: '70vh',
         }}
       />
