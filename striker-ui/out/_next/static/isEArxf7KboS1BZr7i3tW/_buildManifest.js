a5e0c85d405ffcf30a3300424365ab35ebc9d366
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2021-06-25 17:18:08 -0400

    chore: rebuild web UI

--- striker-ui/out/_next/static/isEArxf7KboS1BZr7i3tW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/576-8c091667959361a02026.js","static/chunks/pages/index-312a12570203472e8a0b.js"],"/_error":["static/chunks/pages/_error-a9f53acb468cbab8a6cb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
