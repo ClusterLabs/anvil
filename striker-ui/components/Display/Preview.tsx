66c0e01b27bb59b350292b04bb0ee9472b497a72
Josue <josue.quilon-barrios@senecacollege.ca>
2021-07-06 17:53:54 -0400

    refactor(front-end): add placeholder for preview image and button to switch to full display mode

--- striker-ui/components/Display/Preview.tsx
+++ striker-ui/components/Display/Preview.tsx
@@ -1,20 +1,57 @@
-import VncDisplay from 'react-vnc-display';
+import { Dispatch, SetStateAction } from 'react';
+import Image from 'next/image';
+import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import DesktopWindowsIcon from '@material-ui/icons/DesktopWindows';
 import { Panel } from '../Panels';
-import { HeaderText } from '../Text';
+import { BLACK, TEXT } from '../../lib/consts/DEFAULT_THEME';
 
-const Display = (): JSX.Element => {
+interface PreviewProps {
+  setMode: Dispatch<SetStateAction<boolean>>;
+}
+
+const useStyles = makeStyles(() => ({
+  displayBox: {
+    paddingTop: '1em',
+    paddingBottom: 0,
+  },
+  fullScreenButton: {
+    borderRadius: 8,
+    backgroundColor: TEXT,
+    '&:hover': {
+      backgroundColor: TEXT,
+    },
+  },
+  fullScreenBox: {
+    paddingLeft: '.7em',
+    paddingRight: 0,
+  },
+}));
+
+const Preview = ({ setMode }: PreviewProps): JSX.Element => {
+  const classes = useStyles();
+
   return (
     <Panel>
-      <HeaderText text="Display" />
-      <VncDisplay
-        url="wss://spain.cdot.systems:5000/"
-        style={{
-          width: '50vw',
-          height: '70vh',
-        }}
-      />
+      <Box display="flex" className={classes.displayBox}>
+        <Box flexGrow={1}>
+          <Image src="/pngs/preview.png" width={300} height={200} />
+        </Box>
+        <Box className={classes.fullScreenBox}>
+          <IconButton
+            className={classes.fullScreenButton}
+            style={{ color: BLACK }}
+            aria-label="upload picture"
+            component="span"
+            onClick={() => setMode(false)}
+          >
+            <DesktopWindowsIcon />
+          </IconButton>
+        </Box>
+      </Box>
     </Panel>
   );
 };
 
-export default Display;
+export default Preview;
