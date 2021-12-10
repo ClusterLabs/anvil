5d0dbe3c5f40b8f8068898c76c2d0697a77185ce
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2021-12-10 16:19:35 -0500

    fix(striker-ui-api): enable CORS for all origins

--- striker-ui-api/app.js
+++ striker-ui-api/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 
@@ -9,6 +10,7 @@ const filesRouter = require('./routes/files');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(path.join(API_ROOT_PATH, 'echo'), echoRouter);
 app.use(path.join(API_ROOT_PATH, 'files'), filesRouter);
