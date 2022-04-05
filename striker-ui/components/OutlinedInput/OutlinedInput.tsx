c1f783286ec50b4d7ac53779fee262ff5f26e095
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2022-04-05 17:48:05 -0400

    fix(striker-ui): allow OutlinedInput style override

--- striker-ui/components/OutlinedInput/OutlinedInput.tsx
+++ striker-ui/components/OutlinedInput/OutlinedInput.tsx
@@ -1,35 +1,52 @@
 import {
   OutlinedInput as MUIOutlinedInput,
   outlinedInputClasses as muiOutlinedInputClasses,
-  styled,
+  OutlinedInputProps as MUIOutlinedInputProps,
 } from '@mui/material';
 
 import { GREY, TEXT, UNSELECTED } from '../../lib/consts/DEFAULT_THEME';
 
-const OutlinedInput = styled(MUIOutlinedInput)({
-  color: GREY,
+const OutlinedInput = (
+  outlinedInputProps: MUIOutlinedInputProps,
+): JSX.Element => {
+  const { label, sx } = outlinedInputProps;
+  const combinedSx = {
+    color: GREY,
 
-  [`& .${muiOutlinedInputClasses.notchedOutline}`]: {
-    borderColor: UNSELECTED,
-  },
-
-  '&:hover': {
     [`& .${muiOutlinedInputClasses.notchedOutline}`]: {
-      borderColor: GREY,
+      borderColor: UNSELECTED,
     },
-  },
 
-  [`&.${muiOutlinedInputClasses.focused}`]: {
-    color: TEXT,
-
-    [`& .${muiOutlinedInputClasses.notchedOutline}`]: {
-      borderColor: GREY,
-
-      '& legend': {
-        paddingRight: '1.2em',
+    '&:hover': {
+      [`& .${muiOutlinedInputClasses.notchedOutline}`]: {
+        borderColor: GREY,
       },
     },
-  },
-});
+
+    [`&.${muiOutlinedInputClasses.focused}`]: {
+      color: TEXT,
+
+      [`& .${muiOutlinedInputClasses.notchedOutline}`]: {
+        borderColor: GREY,
+
+        '& legend': {
+          paddingRight: label ? '1.2em' : 0,
+        },
+      },
+    },
+
+    ...sx,
+  };
+
+  return (
+    <MUIOutlinedInput
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...{
+        ...outlinedInputProps,
+        sx: combinedSx,
+      }}
+    />
+  );
+};
 
 export default OutlinedInput;
