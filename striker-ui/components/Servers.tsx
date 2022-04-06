a44d50345d78f7b9019916ba002577d724f91e96
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2022-04-06 01:05:14 -0400

    feat(striker-ui): add ProvisionServerDialog WIP

--- striker-ui/components/Servers.tsx
+++ striker-ui/components/Servers.tsx
@@ -40,6 +40,7 @@ import { BodyText, HeaderText } from './Text';
 import hostsSanitizer from '../lib/sanitizers/hostsSanitizer';
 import periodicFetch from '../lib/fetchers/periodicFetch';
 import putFetch from '../lib/fetchers/putFetch';
+import ProvisionServerDialog from './ProvisionServerDialog';
 
 const PREFIX = 'Servers';
 
@@ -172,6 +173,8 @@ const Servers = ({ anvil }: { anvil: AnvilListItem[] }): JSX.Element => {
   const [showCheckbox, setShowCheckbox] = useState<boolean>(false);
   const [allSelected, setAllSelected] = useState<boolean>(false);
   const [selected, setSelected] = useState<string[]>([]);
+  const [isOpenProvisionServerDialog, setIsOpenProvisionServerDialog] =
+    useState<boolean>(false);
 
   const { uuid } = useContext(AnvilContext);
 
@@ -230,165 +233,175 @@ const Servers = ({ anvil }: { anvil: AnvilListItem[] }): JSX.Element => {
   const filteredHosts = hostsSanitizer(anvil[anvilIndex]?.hosts);
 
   return (
-    <Panel>
-      <StyledDiv>
-        <PanelHeader className={classes.headerPadding} sx={{ marginBottom: 0 }}>
-          <HeaderText text="Servers" />
-          <IconButton>
-            <AddIcon />
-          </IconButton>
-          <IconButton onClick={() => setShowCheckbox(!showCheckbox)}>
-            {showCheckbox ? <CheckIcon sx={{ color: BLUE }} /> : <EditIcon />}
-          </IconButton>
-        </PanelHeader>
-        {showCheckbox && (
-          <>
-            <Box className={classes.headerPadding} display="flex">
-              <Box flexGrow={1} className={classes.dropdown}>
-                <Button
-                  variant="contained"
-                  startIcon={<MoreVertIcon />}
-                  onClick={handleClick}
-                  className={classes.serverActionButton}
-                >
-                  <Typography className={classes.power} variant="subtitle1">
-                    Power
-                  </Typography>
-                </Button>
-                <Menu
-                  anchorEl={anchorEl}
-                  keepMounted
-                  open={Boolean(anchorEl)}
-                  onClose={() => setAnchorEl(null)}
-                >
-                  {buttonLabels.current.map((label: ButtonLabels) => (
-                    <ServerActionButtonMenuItem
-                      onClick={() => handlePower(label)}
-                      key={label}
-                    >
-                      <ServerActionButtonMenuItemLabel
-                        className={classes[label]}
-                        variant="subtitle1"
-                      >
-                        {label.replace(/^[a-z]/, (c) => c.toUpperCase())}
-                      </ServerActionButtonMenuItemLabel>
-                    </ServerActionButtonMenuItem>
-                  ))}
-                </Menu>
-              </Box>
-            </Box>
-            <Box display="flex">
-              <Box>
-                <Checkbox
-                  style={{ color: TEXT }}
-                  color="secondary"
-                  checked={allSelected}
-                  onChange={() => {
-                    if (!allSelected) {
-                      setButtons(data.servers);
-                      setSelected(
-                        data.servers.map(
-                          (server: AnvilServer) => server.server_uuid,
-                        ),
-                      );
-                    } else {
-                      setButtons([]);
-                      setSelected([]);
-                    }
-
-                    setAllSelected(!allSelected);
-                  }}
-                />
-              </Box>
-              <Box className={classes.all}>
-                <BodyText text="All" />
-              </Box>
-            </Box>
-          </>
-        )}
-        {!isLoading ? (
-          <Box className={classes.root}>
-            <List component="nav">
-              {data?.servers.map((server: AnvilServer) => (
-                <>
-                  <ListItem
-                    button
-                    className={classes.button}
-                    key={server.server_uuid}
-                    component={showCheckbox ? 'div' : 'a'}
-                    href={`/server?uuid=${server.server_uuid}&server_name=${server.server_name}`}
-                    onClick={() => handleChange(server.server_uuid)}
+    <>
+      <Panel>
+        <StyledDiv>
+          <PanelHeader
+            className={classes.headerPadding}
+            sx={{ marginBottom: 0 }}
+          >
+            <HeaderText text="Servers" />
+            <IconButton onClick={() => setIsOpenProvisionServerDialog(true)}>
+              <AddIcon />
+            </IconButton>
+            <IconButton onClick={() => setShowCheckbox(!showCheckbox)}>
+              {showCheckbox ? <CheckIcon sx={{ color: BLUE }} /> : <EditIcon />}
+            </IconButton>
+          </PanelHeader>
+          {showCheckbox && (
+            <>
+              <Box className={classes.headerPadding} display="flex">
+                <Box flexGrow={1} className={classes.dropdown}>
+                  <Button
+                    variant="contained"
+                    startIcon={<MoreVertIcon />}
+                    onClick={handleClick}
+                    className={classes.serverActionButton}
                   >
-                    <Box display="flex" flexDirection="row" width="100%">
-                      {showCheckbox && (
-                        <Box className={classes.checkbox}>
-                          <Checkbox
-                            style={{ color: TEXT }}
-                            color="secondary"
-                            checked={
-                              selected.find((s) => s === server.server_uuid) !==
-                              undefined
+                    <Typography className={classes.power} variant="subtitle1">
+                      Power
+                    </Typography>
+                  </Button>
+                  <Menu
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={() => setAnchorEl(null)}
+                  >
+                    {buttonLabels.current.map((label: ButtonLabels) => (
+                      <ServerActionButtonMenuItem
+                        onClick={() => handlePower(label)}
+                        key={label}
+                      >
+                        <ServerActionButtonMenuItemLabel
+                          className={classes[label]}
+                          variant="subtitle1"
+                        >
+                          {label.replace(/^[a-z]/, (c) => c.toUpperCase())}
+                        </ServerActionButtonMenuItemLabel>
+                      </ServerActionButtonMenuItem>
+                    ))}
+                  </Menu>
+                </Box>
+              </Box>
+              <Box display="flex">
+                <Box>
+                  <Checkbox
+                    style={{ color: TEXT }}
+                    color="secondary"
+                    checked={allSelected}
+                    onChange={() => {
+                      if (!allSelected) {
+                        setButtons(data.servers);
+                        setSelected(
+                          data.servers.map(
+                            (server: AnvilServer) => server.server_uuid,
+                          ),
+                        );
+                      } else {
+                        setButtons([]);
+                        setSelected([]);
+                      }
+
+                      setAllSelected(!allSelected);
+                    }}
+                  />
+                </Box>
+                <Box className={classes.all}>
+                  <BodyText text="All" />
+                </Box>
+              </Box>
+            </>
+          )}
+          {!isLoading ? (
+            <Box className={classes.root}>
+              <List component="nav">
+                {data?.servers.map((server: AnvilServer) => (
+                  <>
+                    <ListItem
+                      button
+                      className={classes.button}
+                      key={server.server_uuid}
+                      component={showCheckbox ? 'div' : 'a'}
+                      href={`/server?uuid=${server.server_uuid}&server_name=${server.server_name}`}
+                      onClick={() => handleChange(server.server_uuid)}
+                    >
+                      <Box display="flex" flexDirection="row" width="100%">
+                        {showCheckbox && (
+                          <Box className={classes.checkbox}>
+                            <Checkbox
+                              style={{ color: TEXT }}
+                              color="secondary"
+                              checked={
+                                selected.find(
+                                  (s) => s === server.server_uuid,
+                                ) !== undefined
+                              }
+                            />
+                          </Box>
+                        )}
+                        <Box p={1}>
+                          <Decorator
+                            colour={selectDecorator(server.server_state)}
+                          />
+                        </Box>
+                        <Box p={1} flexGrow={1}>
+                          <BodyText text={server.server_name} />
+                          <BodyText
+                            text={
+                              serverState.get(server.server_state) ||
+                              'Not Available'
                             }
                           />
                         </Box>
-                      )}
-                      <Box p={1}>
-                        <Decorator
-                          colour={selectDecorator(server.server_state)}
-                        />
+                        <Box display="flex" className={classes.hostsBox}>
+                          {server.server_state !== 'shut off' &&
+                            server.server_state !== 'crashed' &&
+                            filteredHosts.map(
+                              (
+                                host: AnvilStatusHost,
+                                index: number,
+                              ): JSX.Element => (
+                                <>
+                                  <Box
+                                    p={1}
+                                    key={host.host_uuid}
+                                    className={classes.hostBox}
+                                  >
+                                    <BodyText
+                                      text={host.host_name}
+                                      selected={
+                                        server.server_host_uuid ===
+                                        host.host_uuid
+                                      }
+                                    />
+                                  </Box>
+                                  {index !== filteredHosts.length - 1 && (
+                                    <Divider
+                                      className={`${classes.divider} ${classes.verticalDivider}`}
+                                      orientation="vertical"
+                                    />
+                                  )}
+                                </>
+                              ),
+                            )}
+                        </Box>
                       </Box>
-                      <Box p={1} flexGrow={1}>
-                        <BodyText text={server.server_name} />
-                        <BodyText
-                          text={
-                            serverState.get(server.server_state) ||
-                            'Not Available'
-                          }
-                        />
-                      </Box>
-                      <Box display="flex" className={classes.hostsBox}>
-                        {server.server_state !== 'shut off' &&
-                          server.server_state !== 'crashed' &&
-                          filteredHosts.map(
-                            (
-                              host: AnvilStatusHost,
-                              index: number,
-                            ): JSX.Element => (
-                              <>
-                                <Box
-                                  p={1}
-                                  key={host.host_uuid}
-                                  className={classes.hostBox}
-                                >
-                                  <BodyText
-                                    text={host.host_name}
-                                    selected={
-                                      server.server_host_uuid === host.host_uuid
-                                    }
-                                  />
-                                </Box>
-                                {index !== filteredHosts.length - 1 && (
-                                  <Divider
-                                    className={`${classes.divider} ${classes.verticalDivider}`}
-                                    orientation="vertical"
-                                  />
-                                )}
-                              </>
-                            ),
-                          )}
-                      </Box>
-                    </Box>
-                  </ListItem>
-                  <Divider className={classes.divider} />
-                </>
-              ))}
-            </List>
-          </Box>
-        ) : (
-          <Spinner />
-        )}
-      </StyledDiv>
-    </Panel>
+                    </ListItem>
+                    <Divider className={classes.divider} />
+                  </>
+                ))}
+              </List>
+            </Box>
+          ) : (
+            <Spinner />
+          )}
+        </StyledDiv>
+      </Panel>
+      <ProvisionServerDialog
+        dialogProps={{ open: isOpenProvisionServerDialog }}
+      />
+    </>
   );
 };
 
