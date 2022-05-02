0da985a625041c18eb5811ab262eba1ebb18a991
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2022-05-02 14:40:31 -0400

    fix(striker-ui): add button to clear Select

--- striker-ui/components/Select.tsx
+++ striker-ui/components/Select.tsx
@@ -1,19 +1,62 @@
+import { FC } from 'react';
 import {
+  IconButton as MUIIconButton,
+  IconButtonProps as MUIIconButtonProps,
+  iconButtonClasses as muiIconButtonClasses,
+  inputClasses,
   Select as MUISelect,
   selectClasses as muiSelectClasses,
   SelectProps as MUISelectProps,
+  InputAdornment as MUIInputAdornment,
+  inputAdornmentClasses as muiInputAdornmentClasses,
 } from '@mui/material';
+import { Close as CloseIcon } from '@mui/icons-material';
 
 import { GREY } from '../lib/consts/DEFAULT_THEME';
 
-type SelectProps = MUISelectProps;
+type SelectOptionalProps = {
+  onClearIndicatorClick?: MUIIconButtonProps['onClick'] | null;
+};
 
-const Select = (selectProps: SelectProps): JSX.Element => {
-  const { children, sx } = selectProps;
+type SelectProps = MUISelectProps & SelectOptionalProps;
+
+const SELECT_DEFAULT_PROPS: Required<SelectOptionalProps> = {
+  onClearIndicatorClick: null,
+};
+
+const Select: FC<SelectProps> = (selectProps) => {
+  const {
+    onClearIndicatorClick = SELECT_DEFAULT_PROPS.onClearIndicatorClick,
+    ...muiSelectProps
+  } = selectProps;
+  const { children, sx } = muiSelectProps;
+  const clearIndicator: JSX.Element | undefined = onClearIndicatorClick ? (
+    <MUIInputAdornment position="end">
+      <MUIIconButton onClick={onClearIndicatorClick}>
+        <CloseIcon fontSize="small" />
+      </MUIIconButton>
+    </MUIInputAdornment>
+  ) : undefined;
+
   const combinedSx = {
     [`& .${muiSelectClasses.icon}`]: {
       color: GREY,
     },
+
+    [`& .${muiInputAdornmentClasses.root}`]: {
+      marginRight: '.8em',
+    },
+
+    [`& .${muiIconButtonClasses.root}`]: {
+      color: GREY,
+      visibility: 'hidden',
+    },
+
+    [`&:hover .${muiInputAdornmentClasses.root} .${muiIconButtonClasses.root},
+      &.${inputClasses.focused} .${muiInputAdornmentClasses.root} .${muiIconButtonClasses.root}`]:
+      {
+        visibility: 'visible',
+      },
 
     ...sx,
   };
@@ -22,7 +65,8 @@ const Select = (selectProps: SelectProps): JSX.Element => {
     <MUISelect
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...{
-        ...selectProps,
+        endAdornment: clearIndicator,
+        ...muiSelectProps,
         sx: combinedSx,
       }}
     >
@@ -30,6 +74,8 @@ const Select = (selectProps: SelectProps): JSX.Element => {
     </MUISelect>
   );
 };
+
+Select.defaultProps = SELECT_DEFAULT_PROPS;
 
 export type { SelectProps };
 
