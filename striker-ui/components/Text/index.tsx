29cd0d2c2f32e0fbb70aadeb3e6c98f89b683644
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2022-07-08 15:39:02 -0400

    fix(striker-ui): change NIC table render

--- striker-ui/components/Text/index.tsx
+++ striker-ui/components/Text/index.tsx
@@ -1,5 +1,6 @@
-import BodyText from './BodyText';
+import BodyText, { BodyTextProps } from './BodyText';
 import HeaderText from './HeaderText';
 import Monospace from './Monospace';
 
+export type { BodyTextProps };
 export { BodyText, HeaderText, Monospace };
