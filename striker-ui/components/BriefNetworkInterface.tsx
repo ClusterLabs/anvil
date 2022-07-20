cf52423ead15507c77fb543bbb42717c75f8ef60
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2022-07-20 19:39:38 -0400

    fix(striker-ui): make floating NIC more visible

--- striker-ui/components/BriefNetworkInterface.tsx
+++ striker-ui/components/BriefNetworkInterface.tsx
@@ -7,12 +7,13 @@ import {
 } from '@mui/material';
 import { Close as MUICloseIcon } from '@mui/icons-material';
 
-import { GREY } from '../lib/consts/DEFAULT_THEME';
+import { BLACK, BORDER_RADIUS, GREY } from '../lib/consts/DEFAULT_THEME';
 
 import Decorator from './Decorator';
 import { BodyText } from './Text';
 
 type BriefNetworkInterfaceOptionalProps = {
+  isFloating?: boolean;
   onClose?: MUIIconButtonProps['onClick'];
 };
 
@@ -20,6 +21,7 @@ const BRIEF_NETWORK_INTERFACE_DEFAULT_PROPS: Required<
   Omit<BriefNetworkInterfaceOptionalProps, 'onClose'>
 > &
   Pick<BriefNetworkInterfaceOptionalProps, 'onClose'> = {
+  isFloating: false,
   onClose: undefined,
 };
 
@@ -29,40 +31,55 @@ const BriefNetworkInterface: FC<
       networkInterface: NetworkInterfaceOverviewMetadata;
     }
 > = ({
+  isFloating,
   networkInterface: { networkInterfaceName, networkInterfaceState },
   onClose,
   sx: rootSx,
   ...restRootProps
-}) => (
-  <MUIBox
-    {...{
-      sx: {
-        display: 'flex',
-        flexDirection: 'row',
+}) => {
+  const draggingSx: MUIBoxProps['sx'] = isFloating
+    ? {
+        borderColor: GREY,
+        borderRadius: BORDER_RADIUS,
+        borderStyle: 'solid',
+        borderWidth: '1px',
+        backgroundColor: BLACK,
+        padding: '.6em 1.2em',
+      }
+    : {};
 
-        '& > :not(:first-child)': {
-          alignSelf: 'center',
-          marginLeft: '.5em',
+  return (
+    <MUIBox
+      {...{
+        sx: {
+          display: 'flex',
+          flexDirection: 'row',
+
+          '& > :not(:first-child)': {
+            alignSelf: 'center',
+            marginLeft: '.5em',
+          },
+
+          ...draggingSx,
+          ...rootSx,
         },
 
-        ...rootSx,
-      },
-
-      ...restRootProps,
-    }}
-  >
-    <Decorator
-      colour={networkInterfaceState === 'up' ? 'ok' : 'off'}
-      sx={{ height: 'auto' }}
-    />
-    <BodyText text={networkInterfaceName} />
-    {onClose && (
-      <MUIIconButton onClick={onClose} size="small" sx={{ color: GREY }}>
-        <MUICloseIcon />
-      </MUIIconButton>
-    )}
-  </MUIBox>
-);
+        ...restRootProps,
+      }}
+    >
+      <Decorator
+        colour={networkInterfaceState === 'up' ? 'ok' : 'off'}
+        sx={{ height: 'auto' }}
+      />
+      <BodyText text={networkInterfaceName} />
+      {onClose && (
+        <MUIIconButton onClick={onClose} size="small" sx={{ color: GREY }}>
+          <MUICloseIcon />
+        </MUIIconButton>
+      )}
+    </MUIBox>
+  );
+};
 
 BriefNetworkInterface.defaultProps = BRIEF_NETWORK_INTERFACE_DEFAULT_PROPS;
 
