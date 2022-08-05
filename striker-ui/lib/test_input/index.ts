e489655857d3f656ec911b3f1828d788bbab91d0
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2022-08-05 17:33:49 -0400

    fix(striker-ui): export testLength() in barrel

--- striker-ui/lib/test_input/index.ts
+++ striker-ui/lib/test_input/index.ts
@@ -1,6 +1,7 @@
 import testInput from './testInput';
+import testLength from './testLength';
 import testMax from './testMax';
 import testNotBlank from './testNotBlank';
 import testRange from './testRange';
 
-export { testInput, testMax, testNotBlank, testRange };
+export { testInput, testLength, testMax, testNotBlank, testRange };
