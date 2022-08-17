1586202deafbf48e59384d7fdd2feaa73f0d23e5
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2022-08-17 19:07:52 -0400

    fix(striker-ui): allow specify options in create input test function

--- striker-ui/lib/test_input/createTestInputFunction.ts
+++ striker-ui/lib/test_input/createTestInputFunction.ts
@@ -2,13 +2,26 @@ import testInput from './testInput';
 import {
   InputTestBatches,
   TestInputFunction,
+  TestInputFunctionOptions,
 } from '../../types/TestInputFunction';
 
 const createTestInputFunction =
-  (tests: InputTestBatches) =>
   (
-    ...[options, ...restArgs]: Parameters<TestInputFunction>
-  ): ReturnType<TestInputFunction> =>
-    testInput({ tests, ...options }, ...restArgs);
+    tests: InputTestBatches,
+    {
+      excludeTestIds: defaultExcludeTestIds = [],
+      ...restDefaultOptions
+    }: Omit<TestInputFunctionOptions, 'inputs' | 'tests'> = {},
+  ) =>
+  ({
+    excludeTestIds = [],
+    ...restOptions
+  }: Parameters<TestInputFunction>[0] = {}): ReturnType<TestInputFunction> =>
+    testInput({
+      tests,
+      excludeTestIds: [...defaultExcludeTestIds, ...excludeTestIds],
+      ...restDefaultOptions,
+      ...restOptions,
+    });
 
 export default createTestInputFunction;
