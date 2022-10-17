ce0bf93c12e22bcf816d688d959bdcbd6ac71b33
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2022-10-17 16:03:39 -0400

    feat(striker-ui): add styled Divider

---
+++ striker-ui/components/Divider.tsx
@@ -0,0 +1,9 @@
+import { Divider as MUIDivider, styled } from '@mui/material';
+
+import { DIVIDER } from '../lib/consts/DEFAULT_THEME';
+
+const Divider = styled(MUIDivider)({
+  backgroundColor: DIVIDER,
+});
+
+export default Divider;
