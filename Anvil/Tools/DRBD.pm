61500ef7f5f75046c62fa7dd28032d520b9c7a5d
Digimer <digimer@alteeve.ca>
2022-11-30 16:13:38 -0500

    * This completes the rework of DRBD triggered fencing to use / clear location constraints instead of triggering a power fence. * Added the new unfence_pacemaker DRBD unfence handler.
    Signed-off-by: Digimer <digimer@alteeve.ca>

--- Anvil/Tools/DRBD.pm
+++ Anvil/Tools/DRBD.pm
@@ -2755,6 +2755,7 @@ sub update_global_common
 	my $usage_count_seen         = 0;
 	my $udev_always_use_vnr_seen = 0;
 	my $fence_peer_seen          = 0;
+	my $unfence_peer_seen        = 0;
 	my $auto_promote_seen        = 0;
 	my $disk_flushes_seen        = 0;
 	my $md_flushes_seen          = 0;
@@ -2778,6 +2779,7 @@ sub update_global_common
 	# These values will be used to track where we are in processing the config file and what values are needed.
 	my $say_usage_count         = $usage_count ? "yes" : "no";
 	my $say_fence_peer          = $anvil->data->{path}{exe}{fence_pacemaker};
+	my $say_unfence_peer        = $anvil->data->{path}{exe}{unfence_pacemaker};
 	my $say_auto_promote        = "yes";
 	my $say_flushes             = $use_flushes ? "yes" : "no";
 	my $say_allow_two_primaries = "no";
@@ -2858,6 +2860,17 @@ sub update_global_common
 					{
 						   $update   = 1;
 						my $new_line = "\t\tfence-peer ".$say_fence_peer.";";
+						$anvil->Log->variables({source => $THIS_FILE, line => __LINE__, level => $debug, list => { 
+							's1:update'   => $update,
+							's2:new_line' => $new_line, 
+						}});
+						$new_global_common .= $new_line."\n";
+						$anvil->Log->entry({source => $THIS_FILE, line => __LINE__, level => $debug, key => "log_0518", variables => { file => $anvil->data->{path}{configs}{'global-common.conf'}, line => $line }});
+					}
+					if (not $unfence_peer_seen)
+					{
+						   $update   = 1;
+						my $new_line = "\t\tunfence-peer ".$say_unfence_peer.";";
 						$anvil->Log->variables({source => $THIS_FILE, line => __LINE__, level => $debug, list => { 
 							's1:update'   => $update,
 							's2:new_line' => $new_line, 
@@ -3110,6 +3123,35 @@ sub update_global_common
 				{
 					   $update   = 1;
 					my $new_line = $left_space."fence-peer".$middle_space.$say_fence_peer.$right_side;
+					$anvil->Log->variables({source => $THIS_FILE, line => __LINE__, level => $debug, list => { 
+						's1:update'   => $update,
+						's2:new_line' => $new_line, 
+					}});
+					
+					$new_global_common .= $new_line.$comment."\n";
+					$anvil->Log->entry({source => $THIS_FILE, line => __LINE__, level => $debug, key => "log_0518", variables => { file => $anvil->data->{path}{configs}{'global-common.conf'}, line => $line }});
+					next;
+				}
+			}
+			if ($line =~ /(\s*)unfence-peer(\s+)(.*?)(;.*)$/)
+			{
+				my $left_space        = $1;
+				my $middle_space      = $2;
+				my $value             = $3;
+				my $right_side        = $4;
+				   $unfence_peer_seen = 1;
+				$anvil->Log->variables({source => $THIS_FILE, line => __LINE__, level => $debug, list => { 
+					's1:left_space'        => $left_space,
+					's2:middle_space'      => $middle_space, 
+					's3:value'             => $value, 
+					's4:right_side'        => $right_side,
+					's5:unfence_peer_seen' => $fence_peer_seen, 
+				}});
+				   
+				if ($value ne $say_unfence_peer)
+				{
+					   $update   = 1;
+					my $new_line = $left_space."unfence-peer".$middle_space.$say_unfence_peer.$right_side;
 					$anvil->Log->variables({source => $THIS_FILE, line => __LINE__, level => $debug, list => { 
 						's1:update'   => $update,
 						's2:new_line' => $new_line, 
