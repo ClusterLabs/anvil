a1ba570eee69972212e43b9001665315f1864be1
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-02-17 00:36:39 -0500

    fix(striker-ui): allow retain/discard tab content

--- striker-ui/components/TabContent.tsx
+++ striker-ui/components/TabContent.tsx
@@ -1,21 +1,29 @@
 import { Box } from '@mui/material';
-import { ReactElement, useMemo } from 'react';
+import { ReactElement, ReactNode, useMemo } from 'react';
 
 const TabContent = <T,>({
   changingTabId,
   children,
+  retain = false,
   tabId,
 }: TabContentProps<T>): ReactElement => {
   const isTabIdMatch = useMemo(
     () => changingTabId === tabId,
     [changingTabId, tabId],
   );
-  const displayValue = useMemo(
-    () => (isTabIdMatch ? 'initial' : 'none'),
-    [isTabIdMatch],
+  const result = useMemo<ReactNode>(
+    () =>
+      retain ? (
+        <Box sx={{ display: isTabIdMatch ? 'initial' : 'none' }}>
+          {children}
+        </Box>
+      ) : (
+        isTabIdMatch && children
+      ),
+    [children, isTabIdMatch, retain],
   );
 
-  return <Box sx={{ display: displayValue }}>{children}</Box>;
+  return <>{result}</>;
 };
 
 export default TabContent;
