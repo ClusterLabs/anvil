bea7ad25c6b5e36736e1052a502673780543fb7e
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-02-17 22:08:15 -0500

    fix(striker-ui): enable pass previous values to CommonFenceInputGroup

--- striker-ui/components/CommonFenceInputGroup.tsx
+++ striker-ui/components/CommonFenceInputGroup.tsx
@@ -40,15 +40,33 @@ const MAP_TO_INPUT_BUILDER: MapToInputBuilder = {
   ),
 };
 
+const combineIds = (...pieces: string[]) => pieces.join('-');
+
 const CommonFenceInputGroup: FC<CommonFenceInputGroupProps> = ({
   fenceId,
   fenceTemplate,
+  previousFenceName,
+  previousFenceParameters,
 }) => {
   const fenceParameterElements = useMemo(() => {
     let result: ReactNode;
 
     if (fenceTemplate && fenceId) {
       const { parameters: fenceParameters } = fenceTemplate[fenceId];
+
+      let mapToPreviousFenceParameterValues: FenceParameters = {};
+
+      if (previousFenceParameters) {
+        mapToPreviousFenceParameterValues = Object.entries(
+          previousFenceParameters,
+        ).reduce<FenceParameters>((previous, [parameterId, parameterValue]) => {
+          const newKey = combineIds(fenceId, parameterId);
+
+          previous[newKey] = parameterValue;
+
+          return previous;
+        }, {});
+      }
 
       const { optional: optionalInputs, required: requiredInputs } =
         Object.entries(fenceParameters).reduce<{
@@ -71,15 +89,18 @@ const CommonFenceInputGroup: FC<CommonFenceInputGroupProps> = ({
             const buildInput =
               MAP_TO_INPUT_BUILDER[contentType] ?? MAP_TO_INPUT_BUILDER.string;
 
-            const fenceJoinParameterId = `${fenceId}-${parameterId}`;
+            const fenceJoinParameterId = combineIds(fenceId, parameterId);
+            const initialValue =
+              mapToPreviousFenceParameterValues[fenceJoinParameterId] ??
+              parameterDefault;
             const parameterIsRequired = isRequired === '1';
             const parameterInput = buildInput({
               id: fenceJoinParameterId,
-              isChecked: CHECKED_STATES.includes(parameterDefault),
+              isChecked: CHECKED_STATES.includes(initialValue),
               isRequired: parameterIsRequired,
               label: parameterId,
               selectOptions: parameterSelectOptions,
-              value: parameterDefault,
+              value: initialValue,
             });
 
             if (parameterIsRequired) {
@@ -94,28 +115,34 @@ const CommonFenceInputGroup: FC<CommonFenceInputGroupProps> = ({
             optional: [],
             required: [
               MAP_TO_INPUT_BUILDER.string({
-                id: `${fenceId}-name`,
+                id: combineIds(fenceId, 'name'),
                 isRequired: true,
                 label: 'Fence device name',
+                value: previousFenceName,
               }),
             ],
           },
         );
 
       result = (
-        <>
+        <FlexBox
+          sx={{
+            '& > div:first-child': { marginTop: 0 },
+            '& > div': { marginBottom: 0 },
+          }}
+        >
           <ExpandablePanel expandInitially header="Required parameters">
             <FlexBox margin=".4em 0">{requiredInputs}</FlexBox>
           </ExpandablePanel>
           <ExpandablePanel header="Optional parameters">
             <FlexBox margin=".4em 0">{optionalInputs}</FlexBox>
           </ExpandablePanel>
-        </>
+        </FlexBox>
       );
     }
 
     return result;
-  }, [fenceId, fenceTemplate]);
+  }, [fenceId, fenceTemplate, previousFenceName, previousFenceParameters]);
 
   return <>{fenceParameterElements}</>;
 };
