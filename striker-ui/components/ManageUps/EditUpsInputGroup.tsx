7976170ca25290628fb4360928a12748f3b4e919
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-03-03 21:16:25 -0500

    fix(striker-ui): passthrough input validation in EditUpsInputGroup

--- striker-ui/components/ManageUps/EditUpsInputGroup.tsx
+++ striker-ui/components/ManageUps/EditUpsInputGroup.tsx
@@ -1,27 +1,40 @@
-import { FC, ReactElement, useMemo } from 'react';
+import { ReactElement, useMemo } from 'react';
 
-import AddUpsInputGroup from './AddUpsInputGroup';
+import AddUpsInputGroup, { INPUT_ID_UPS_TYPE } from './AddUpsInputGroup';
+import { INPUT_ID_UPS_IP, INPUT_ID_UPS_NAME } from './CommonUpsInputGroup';
 import Spinner from '../Spinner';
 
 const INPUT_ID_UPS_UUID = 'edit-ups-input-ups-uuid';
 
-const EditUpsInputGroup: FC<EditUpsInputGroupProps> = ({
+const EditUpsInputGroup = <
+  M extends {
+    [K in
+      | typeof INPUT_ID_UPS_IP
+      | typeof INPUT_ID_UPS_NAME
+      | typeof INPUT_ID_UPS_TYPE]: string;
+  },
+>({
+  formUtils,
   loading: isExternalLoading,
   previous,
   upsTemplate,
   upsUUID,
-}) => {
+}: EditUpsInputGroupProps<M>): ReactElement => {
   const content = useMemo<ReactElement>(
     () =>
       isExternalLoading ? (
         <Spinner />
       ) : (
         <>
-          <AddUpsInputGroup previous={previous} upsTemplate={upsTemplate} />
+          <AddUpsInputGroup
+            formUtils={formUtils}
+            previous={previous}
+            upsTemplate={upsTemplate}
+          />
           <input hidden id={INPUT_ID_UPS_UUID} readOnly value={upsUUID} />
         </>
       ),
-    [isExternalLoading, previous, upsTemplate, upsUUID],
+    [formUtils, isExternalLoading, previous, upsTemplate, upsUUID],
   );
 
   return content;
--- striker-ui/types/EditUpsInputGroup.d.ts
+++ striker-ui/types/EditUpsInputGroup.d.ts
@@ -2,7 +2,8 @@ type EditUpsInputGroupOptionalProps = {
   loading?: boolean;
 };
 
-type EditUpsInputGroupProps = EditUpsInputGroupOptionalProps &
-  Pick<AddUpsInputGroupProps, 'previous' | 'upsTemplate'> & {
-    upsUUID: string;
-  };
+type EditUpsInputGroupProps<M extends MapToInputTestID> =
+  EditUpsInputGroupOptionalProps &
+    Pick<AddUpsInputGroupProps<M>, 'formUtils' | 'previous' | 'upsTemplate'> & {
+      upsUUID: string;
+    };
