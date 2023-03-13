fb8e92ffb47f7e4c312a19a339ecff64372c1e06
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-03-13 16:10:18 -0400

    fix(striker-ui): consolidate network types

--- striker-ui/lib/consts/NETWORK_TYPES.ts
+++ striker-ui/lib/consts/NETWORK_TYPES.ts
@@ -1,6 +1,8 @@
 const NETWORK_TYPES: Record<string, string> = {
   bcn: 'Back-Channel Network',
   ifn: 'Internet-Facing Network',
+  mn: 'Migration Network',
+  sn: 'Storage Network',
 };
 
 export default NETWORK_TYPES;
