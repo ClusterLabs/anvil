8154fd4dbd148770cc12b135396148e3a4b13b21
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-03-15 19:21:44 -0400

    fix(striker-ui): allow remove key in object setter builder

--- striker-ui/lib/buildObjectStateSetterCallback.ts
+++ striker-ui/lib/buildObjectStateSetterCallback.ts
@@ -1,9 +1,13 @@
 const buildObjectStateSetterCallback =
-  <S extends Record<string, unknown>>(key: keyof S, value: S[keyof S]) =>
-  ({ [key]: toReplace, ...restPrevious }: S): S =>
-    ({
-      ...restPrevious,
-      [key]: value,
-    } as S);
+  <S extends Record<string, unknown>>(key: keyof S, value?: S[keyof S]) =>
+  ({ [key]: toReplace, ...restPrevious }: S): S => {
+    const result = { ...restPrevious } as S;
+
+    if (value !== undefined) {
+      result[key] = value;
+    }
+
+    return result;
+  };
 
 export default buildObjectStateSetterCallback;
