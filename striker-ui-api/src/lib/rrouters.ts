c40b7ff7d4d165a74526f0956d4f70180212abcd
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-04-11 23:59:21 -0400

    fix(striker-ui-api): allow customize handlers per route batch in register routers function

--- striker-ui-api/src/lib/rrouters.ts
+++ striker-ui-api/src/lib/rrouters.ts
@@ -10,13 +10,21 @@ export const rrouters = <
 >(
   app: A,
   union: Readonly<M> | R,
-  { key, route = '/' }: { key?: string; route?: string } = {},
+  {
+    assign = (router) => [router],
+    key,
+    route = '/',
+  }: {
+    assign?: (router: R) => R[];
+    key?: keyof M;
+    route?: string;
+  } = {},
 ) => {
   if ('route' in union) {
     stdout(`Setting up route ${route}`);
-    app.use(route, union as R);
+    app.use(route, ...assign(union as R));
   } else if (key) {
-    rrouters(app, union[key], { route: path.posix.join(route, key) });
+    rrouters(app, union[key], { route: path.posix.join(route, String(key)) });
   } else {
     Object.entries(union).forEach(([extend, subunion]) => {
       rrouters(app, subunion, { route: path.posix.join(route, extend) });
