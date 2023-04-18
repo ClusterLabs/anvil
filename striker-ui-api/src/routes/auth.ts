a4524b6d2758cc952778e40029f7ceb27c839b8c
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-04-18 16:47:26 -0400

    fix(striker-ui-api): add /auth/logout

--- striker-ui-api/src/routes/auth.ts
+++ striker-ui-api/src/routes/auth.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 
-import { login } from '../lib/request_handlers/auth';
+import { assertAuthentication } from '../lib/assertAuthentication';
+import { login, logout } from '../lib/request_handlers/auth';
 import passport from '../passport';
 
 const router = express.Router();
 
-router.post('/login', passport.authenticate('login'), login);
+router
+  .post('/login', passport.authenticate('login'), login)
+  .put('/logout', assertAuthentication(), logout);
 
 export default router;
