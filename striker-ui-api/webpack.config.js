06ad2a53534ae7e6cc1b0679736cbab0fb4908d9
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-04-25 22:41:49 -0400

    chore(striker-ui-api): add core-js and regenerator-runtime enable promises and async/await

--- striker-ui-api/webpack.config.js
+++ striker-ui-api/webpack.config.js
@@ -11,7 +11,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+            presets: [
+              ['@babel/preset-env', { corejs: 3, useBuiltIns: 'usage' }],
+              '@babel/preset-typescript',
+            ],
           },
         },
       },
