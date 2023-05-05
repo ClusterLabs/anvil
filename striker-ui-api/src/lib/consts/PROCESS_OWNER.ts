408e3ba78e0caf08bf4716e540761939c2f1c048
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-05-05 18:42:14 -0400

    fix(striker-ui-api): consolidate config-able constants

--- striker-ui-api/src/lib/consts/PROCESS_OWNER.ts
+++
@@ -1,5 +0,0 @@
-import { resolveGid, resolveUid } from '../shell';
-
-export const PUID = resolveUid(process.env.PUID ?? 'striker-ui-api');
-
-export const PGID = resolveGid(process.env.PGID ?? PUID);
