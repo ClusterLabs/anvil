2f9af328456e0d861fe59a6e3cedb762473c9df7
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-06-20 21:52:06 -0400

    fix(striker-ui): simplify icon links, display session user in drawer

--- striker-ui/components/AnvilDrawer.tsx
+++ striker-ui/components/AnvilDrawer.tsx
@@ -1,11 +1,20 @@
-import { Box, Divider, Drawer, List, ListItem } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import DashboardIcon from '@mui/icons-material/Dashboard';
+import { Dashboard as DashboardIcon } from '@mui/icons-material';
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  styled,
+} from '@mui/material';
 import { Dispatch, SetStateAction } from 'react';
-import { BodyText, HeaderText } from './Text';
-import { ICONS, ICON_SIZE } from '../lib/consts/ICONS';
+
 import { DIVIDER, GREY } from '../lib/consts/DEFAULT_THEME';
-import API_BASE_URL from '../lib/consts/API_BASE_URL';
+import { ICONS, ICON_SIZE } from '../lib/consts/ICONS';
+
+import { BodyText } from './Text';
+import useCookieJar from '../hooks/useCookieJar';
 
 const PREFIX = 'AnvilDrawer';
 
@@ -46,55 +55,54 @@ interface DrawerProps {
   setOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-const AnvilDrawer = ({ open, setOpen }: DrawerProps): JSX.Element => (
-  <StyledDrawer
-    BackdropProps={{ invisible: true }}
-    anchor="left"
-    open={open}
-    onClose={() => setOpen(!open)}
-  >
-    <div role="presentation">
-      <List className={classes.list}>
-        <ListItem button>
-          <HeaderText text="Admin" />
-        </ListItem>
-        <Divider className={classes.divider} />
-        <ListItem button component="a" href="/index.html">
-          <Box display="flex" flexDirection="row" width="100%">
-            <Box className={classes.dashboardButton}>
-              <DashboardIcon className={classes.dashboardIcon} />
-            </Box>
-            <Box flexGrow={1} className={classes.text}>
-              <BodyText text="Dashboard" />
-            </Box>
-          </Box>
-        </ListItem>
-        {ICONS.map(
-          (icon): JSX.Element => (
-            <ListItem
-              button
-              key={icon.image}
-              component="a"
-              href={
-                icon.uri.search(/^https?:/) !== -1
-                  ? icon.uri
-                  : `${API_BASE_URL}${icon.uri}`
-              }
-            >
-              <Box display="flex" flexDirection="row" width="100%">
-                <Box>
-                  <img alt="" key="icon" src={icon.image} {...ICON_SIZE} />
-                </Box>
-                <Box flexGrow={1} className={classes.text}>
-                  <BodyText text={icon.text} />
-                </Box>
+const AnvilDrawer = ({ open, setOpen }: DrawerProps): JSX.Element => {
+  const { getSessionUser } = useCookieJar();
+
+  const sessionUser = getSessionUser();
+
+  return (
+    <StyledDrawer
+      BackdropProps={{ invisible: true }}
+      anchor="left"
+      open={open}
+      onClose={() => setOpen(!open)}
+    >
+      <div role="presentation">
+        <List className={classes.list}>
+          <ListItem>
+            <BodyText>
+              {sessionUser ? <>Welcome, {sessionUser.name}</> : 'Unregistered'}
+            </BodyText>
+          </ListItem>
+          <Divider className={classes.divider} />
+          <ListItemButton component="a" href="/index.html">
+            <Box display="flex" flexDirection="row" width="100%">
+              <Box className={classes.dashboardButton}>
+                <DashboardIcon className={classes.dashboardIcon} />
               </Box>
-            </ListItem>
-          ),
-        )}
-      </List>
-    </div>
-  </StyledDrawer>
-);
+              <Box flexGrow={1} className={classes.text}>
+                <BodyText text="Dashboard" />
+              </Box>
+            </Box>
+          </ListItemButton>
+          {ICONS.map(
+            (icon): JSX.Element => (
+              <ListItemButton key={icon.image} component="a" href={icon.uri}>
+                <Box display="flex" flexDirection="row" width="100%">
+                  <Box>
+                    <img alt="" key="icon" src={icon.image} {...ICON_SIZE} />
+                  </Box>
+                  <Box flexGrow={1} className={classes.text}>
+                    <BodyText text={icon.text} />
+                  </Box>
+                </Box>
+              </ListItemButton>
+            ),
+          )}
+        </List>
+      </div>
+    </StyledDrawer>
+  );
+};
 
 export default AnvilDrawer;
