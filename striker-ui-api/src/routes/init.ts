bc1b8bec205fa131bcbf18461713e25b8b23f5b9
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-06-22 04:07:42 -0400

    fix(striker-ui-api): expose job endpoint for init, filter job by command

--- striker-ui-api/src/routes/init.ts
+++ striker-ui-api/src/routes/init.ts
@@ -4,11 +4,33 @@ import { assertInit } from '../middlewares';
 
 import { setMapNetwork } from '../lib/request_handlers/command';
 import { configStriker } from '../lib/request_handlers/host';
+import { getJob } from '../lib/request_handlers/job';
 import { getNetworkInterface } from '../lib/request_handlers/network-interface';
 
 const router = express.Router();
 
 router
+  .get(
+    '/job',
+    (request, response, next) => {
+      const {
+        path,
+        query: { command, start },
+      } = request;
+
+      if (command) return next();
+
+      return response.redirect(
+        `/api/init${path}?command=anvil-configure-host&start=${start}`,
+      );
+    },
+    assertInit({
+      fail: ({ url }, response) => {
+        response.redirect(307, `/api${url}`);
+      },
+    }),
+    getJob,
+  )
   .get(
     '/network-interface/:hostUUID?',
     assertInit({
