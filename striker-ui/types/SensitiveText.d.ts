0edb8910c2531373d4cc9c323f9dc23d74a3f3f8
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-06-28 20:25:38 -0400

    fix(striker-ui): expose reveal button props, allow adjust line height in SensitiveText

--- striker-ui/types/SensitiveText.d.ts
+++ striker-ui/types/SensitiveText.d.ts
@@ -1,7 +1,9 @@
 type SensitiveTextOptionalProps = {
   inline?: boolean;
   monospaced?: boolean;
+  revealButtonProps?: import('../components/IconButton').IconButtonProps;
   revealInitially?: boolean;
+  textLineHeight?: number | null;
   textProps?: import('../components/Text').BodyTextProps;
 };
 
