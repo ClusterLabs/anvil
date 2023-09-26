bd33f8711f651c69e2163a14cf96fea9df53f10f
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-09-26 02:45:39 -0400

    fix(striker-ui): add function to calculate elapsed string

--- striker-ui/lib/time.ts
+++ striker-ui/lib/time.ts
@@ -20,4 +20,46 @@ const last = (
   return diff <= duration;
 };
 
-export { before, last };
+const elapsed = (
+  duration: number,
+  { ms }: { ms?: boolean } = {},
+): { h: number; m: number; s: number; unit: string; value: number } => {
+  let src = duration;
+
+  if (!ms) {
+    src /= 1000;
+  }
+
+  const parts = [60, 60].reduce<number[]>((previous, multiplier) => {
+    const remainder = src % multiplier;
+
+    previous.push(remainder);
+
+    src = (src - remainder) / multiplier;
+
+    return previous;
+  }, []);
+
+  const [s, m, h] = [...parts, src];
+
+  const significant = [
+    { unit: 'h', value: h },
+    { unit: 'm', value: m },
+  ].reduce(
+    (previous, current) => {
+      const { value } = current;
+
+      return value ? current : previous;
+    },
+    { unit: 's', value: s },
+  );
+
+  return {
+    h,
+    m,
+    s,
+    ...significant,
+  };
+};
+
+export { before, elapsed, last };
