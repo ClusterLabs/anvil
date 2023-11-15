48dfc1b76c9f0e843caa74c9261ae4e45a756803
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-11-15 14:04:27 -0500

    fix(striker-ui): clean up old file manager

--- striker-ui/pages/file-manager/index.tsx
+++ striker-ui/pages/file-manager/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
 import Header from '../../components/Header';
-import ManageFilePanel from '../../components/Files/ManageFilePanel';
+import ManageFilePanel from '../../components/Files';
 
 const FileManager = (): JSX.Element => (
   <>
--- striker-ui/types/FileLocation.d.ts
+++
@@ -1,9 +0,0 @@
-type FileLocation = {
-  anvilName: string;
-  anvilDescription: string;
-  anvilUUID: string;
-  fileLocationUUID: string;
-  hostName: string;
-  hostUUID: string;
-  isFileLocationActive: boolean;
-};
