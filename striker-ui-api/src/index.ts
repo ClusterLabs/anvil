efc3b46c2b38ae00c142e56ffd184d20fc2fd399
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2023-11-17 15:23:51 -0500

    fix(striker-ui-api): wait for access module setup before starting express app

--- striker-ui-api/src/index.ts
+++ striker-ui-api/src/index.ts
@@ -2,28 +2,43 @@ import { getgid, getuid, setgid, setuid } from 'process';
 
 import { PGID, PUID, PORT, ECODE_DROP_PRIVILEGES } from './lib/consts';
 
-import app from './app';
-import { proxyServerVncUpgrade } from './middlewares';
+import { access } from './lib/accessModule';
 import { stderr, stdout } from './lib/shell';
 
-(async () => {
-  stdout(`Starting main process with ownership ${getuid()}:${getgid()}`);
+/**
+ * Wait until the anvil-access-module daemon finishes its setup before doing
+ * anything else.
+ *
+ * Notes:
+ * * The webpackMode directive tells webpack to include the dynamic module into
+ *   the main bundle. Webpack defaults to put such modules in separate files to
+ *   reduce the amount of loading.
+ */
+access.once('active', async () => {
+  const { default: app } = await import(/* webpackMode: "eager" */ './app');
+  const { proxyServerVncUpgrade } = await import(
+    /* webpackMode: "eager" */ './middlewares'
+  );
 
-  const server = (await app).listen(PORT, () => {
-    try {
-      // Group must be set before user to avoid permission error.
-      setgid(PGID);
-      setuid(PUID);
+  (async () => {
+    stdout(`Starting main process with ownership ${getuid()}:${getgid()}`);
 
-      stdout(`Main process ownership changed to ${getuid()}:${getgid()}.`);
-    } catch (error) {
-      stderr(`Failed to change main process ownership; CAUSE: ${error}`);
+    const server = (await app).listen(PORT, () => {
+      try {
+        // Group must be set before user to avoid permission error.
+        setgid(PGID);
+        setuid(PUID);
 
-      process.exit(ECODE_DROP_PRIVILEGES);
-    }
+        stdout(`Main process ownership changed to ${getuid()}:${getgid()}.`);
+      } catch (error) {
+        stderr(`Failed to change main process ownership; CAUSE: ${error}`);
 
-    stdout(`Listening on localhost:${PORT}.`);
-  });
+        process.exit(ECODE_DROP_PRIVILEGES);
+      }
 
-  server.on('upgrade', proxyServerVncUpgrade);
-})();
+      stdout(`Listening on localhost:${PORT}.`);
+    });
+
+    server.on('upgrade', proxyServerVncUpgrade);
+  })();
+});
