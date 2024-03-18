8b0eba2b2eb226fbc12a9dd93e7c6af3e5aad9b3
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2024-03-18 18:53:12 -0400

    fix(striker-ui): default enable ping in add peer striker

--- striker-ui/components/StrikerConfig/AddPeerDialog.tsx
+++ striker-ui/components/StrikerConfig/AddPeerDialog.tsx
@@ -58,7 +58,7 @@ const AddPeerDialog = forwardRef<
   const [formValidity, setFormValidity] = useState<{
     [inputTestID: string]: boolean;
   }>({});
-  const [isEnablePingTest, setIsEnablePingTest] = useState<boolean>(false);
+  const [isEnablePingTest, setIsEnablePingTest] = useState<boolean>(true);
   const [isSubmittingAddPeer, setIsSubmittingAddPeer] =
     useState<boolean>(false);
 
