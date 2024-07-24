135ce6ce9fef3f05af4956a1390c11b27b72f12d
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2024-07-24 15:17:21 -0400

    fix(striker-ui): simplify, add style presets in checkbox

--- striker-ui/components/Checkbox.tsx
+++ striker-ui/components/Checkbox.tsx
@@ -1,24 +1,66 @@
 import {
-  Checkbox as MUICheckbox,
+  Checkbox as MuiCheckbox,
   checkboxClasses as muiCheckboxClasses,
+  styled,
 } from '@mui/material';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 
-import { GREY } from '../lib/consts/DEFAULT_THEME';
+import { BLACK, GREY } from '../lib/consts/DEFAULT_THEME';
 
-const Checkbox: FC<CheckboxProps> = ({ sx, ...checkboxProps }) => (
-  <MUICheckbox
-    {...{
-      ...checkboxProps,
-      sx: {
-        color: GREY,
+const PREFIX = 'Checkbox';
 
-        [`&.${muiCheckboxClasses.checked}`]: { color: GREY },
+const classes = {
+  invert: `${PREFIX}-invert`,
+  thinPadding: `${PREFIX}-thin-padding`,
+};
 
-        ...sx,
-      },
-    }}
-  />
-);
+const BaseStyle = styled(MuiCheckbox)({
+  color: GREY,
+
+  [`&.${muiCheckboxClasses.checked}`]: {
+    color: GREY,
+  },
+
+  [`&.${classes.invert}`]: {
+    color: BLACK,
+
+    [`&.${muiCheckboxClasses.checked}`]: {
+      color: BLACK,
+    },
+  },
+
+  [`&.${classes.thinPadding}`]: {
+    padding: '.2em',
+  },
+});
+
+const Checkbox: FC<CheckboxProps> = (props) => {
+  const {
+    className: baseClassName,
+    invert,
+    thinPadding,
+    ...restBaseProps
+  } = props;
+
+  const className = useMemo(() => {
+    const cls = [];
+
+    if (baseClassName) {
+      cls.push(baseClassName);
+    }
+
+    if (invert) {
+      cls.push(classes.invert);
+    }
+
+    if (thinPadding) {
+      cls.push(classes.thinPadding);
+    }
+
+    return cls.join(' ');
+  }, [baseClassName, invert, thinPadding]);
+
+  return <BaseStyle className={className} {...restBaseProps} />;
+};
 
 export default Checkbox;
