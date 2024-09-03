d955de5ebeac1b985ef012b9fb046721f97d29be
Madison Kelly <mkelly@alteeve.com>
2024-09-02 22:55:06 -0400

    Increased logging to help with debugging.
    Signed-off-by: Madison Kelly <mkelly@alteeve.com>

--- Anvil/Tools.pm
+++ Anvil/Tools.pm
@@ -428,6 +428,9 @@ sub nice_exit
 		's3:line'     => $line,
 	}});
 	
+	# Log our exit code for debug purposes
+	$anvil->Log->variables({source => $THIS_FILE, line => __LINE__, level => 1, list => { exit_code => $exit_code }});
+	
 	# Close the log file.
 	if ($anvil->data->{HANDLE}{'log'}{main})
 	{
