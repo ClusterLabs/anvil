890d42e582cd693338198052ffd6f1e7aa0a152c
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2024-10-01 15:12:43 -0400

    fix(striker-ui-api): always expose /init/job/:uuid, remove /init/job

--- striker-ui-api/src/routes/init.ts
+++ striker-ui-api/src/routes/init.ts
@@ -3,38 +3,13 @@ import express from 'express';
 import { assertInit } from '../middlewares';
 
 import { configStriker } from '../lib/request_handlers/host';
-import { getJob, getJobDetail } from '../lib/request_handlers/job';
+import { getJobDetail } from '../lib/request_handlers/job';
 import { getNetworkInterface } from '../lib/request_handlers/network-interface';
 
 const router = express.Router();
 
 router
-  .get(
-    '/job',
-    (request, response, next) => {
-      const {
-        path,
-        query: { command },
-      } = request;
-
-      const script = 'anvil-configure-host';
-
-      if (command === script) return next();
-
-      return response.redirect(`/api/init${path}?command=${script}`);
-    },
-    assertInit({
-      fail: (request, response, next) => next(),
-    }),
-    getJob,
-  )
-  .get(
-    '/job/:uuid',
-    assertInit({
-      fail: ({ path }, response) => response.redirect(307, `/api${path}`),
-    }),
-    getJobDetail,
-  )
+  .get('/job/:uuid', getJobDetail)
   .get(
     '/network-interface/:hostUUID?',
     assertInit({
