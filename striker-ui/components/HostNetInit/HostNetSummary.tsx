b8227c4dce57416c1e961ecf1fe24edf14d62103
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2024-12-04 17:15:04 -0500

    fix(striker-ui): enhance visibility of incompletes in hot net summary

--- striker-ui/components/HostNetInit/HostNetSummary.tsx
+++ striker-ui/components/HostNetInit/HostNetSummary.tsx
@@ -1,10 +1,19 @@
 import { Grid } from '@mui/material';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 
 import NETWORK_TYPES from '../../lib/consts/NETWORK_TYPES';
 import { REP_UUID } from '../../lib/consts/REG_EXP_PATTERNS';
 
+import MessageBox from '../MessageBox';
 import { BodyText, InlineMonoText, MonoText } from '../Text';
+
+const getNetKey = (id: string) => `network-confirm-${id}`;
+
+const getNetName = (type: string, seq: string) =>
+  `${NETWORK_TYPES[type]} ${seq}`;
+
+const getNetShort = (type: string, seq: string) =>
+  `${type.toUpperCase()}${seq}`;
 
 // This component requires a 2-column grid container as its parent.
 const HostNetSummary = <Values extends HostNetInitFormikExtension>(
@@ -12,40 +21,48 @@ const HostNetSummary = <Values extends HostNetInitFormikExtension>(
 ): ReturnType<FC<HostNetSummaryProps<Values>>> => {
   const { gatewayIface, ifaces, values } = props;
 
+  const nets = useMemo(
+    () => Object.entries<HostNetFormikValues>(values.networkInit.networks),
+    [values.networkInit.networks],
+  );
+
+  const { hasIface, noIface } = useMemo(() => {
+    const hasInterface: string[] = [];
+    const noInterface: string[] = [];
+
+    nets.forEach(([netId, net]) => {
+      const { interfaces } = net;
+
+      if (interfaces.some((value) => REP_UUID.test(value))) {
+        hasInterface.push(netId);
+      } else {
+        noInterface.push(netId);
+      }
+    });
+
+    return { hasIface: hasInterface, noIface: noInterface };
+  }, [nets]);
+
   return (
     <>
       <Grid item sx={{ marginTop: '1.4em' }} xs={2}>
         <BodyText>Networks</BodyText>
       </Grid>
-      {Object.entries(values.networkInit.networks).map((net) => {
-        const [netId, { interfaces, ip, sequence, subnetMask, type }] = net;
+      {hasIface.map((netId) => {
+        const { interfaces, ip, sequence, subnetMask, type } =
+          values.networkInit.networks[netId];
 
-        const baseKey = `network-confirm-${netId}`;
-
-        const label = (
-          <BodyText>
-            {NETWORK_TYPES[type]} {sequence} (
-            <InlineMonoText>
-              {`${type.toUpperCase()}${sequence}`}
-            </InlineMonoText>
-            )
-          </BodyText>
-        );
-
-        if (!interfaces.some((value) => REP_UUID.test(value))) {
-          return (
-            <Grid key={baseKey} item mb="1.4em" xs={1}>
-              {label}
-              <BodyText>Incomplete, discard.</BodyText>
-            </Grid>
-          );
-        }
+        const baseKey = getNetKey(netId);
+        const name = getNetName(type, sequence);
+        const short = getNetShort(type, sequence);
 
         return (
           <Grid key={baseKey} item mb="1.4em" xs={1}>
             <Grid container spacing=".6em" columns={2}>
               <Grid item width="100%">
-                {label}
+                <BodyText>
+                  {name} (<InlineMonoText>{short}</InlineMonoText>)
+                </BodyText>
               </Grid>
               {interfaces.map((ifaceUuid, ifaceIndex) => {
                 let key = `${baseKey}-interface${ifaceIndex}`;
@@ -71,6 +88,25 @@ const HostNetSummary = <Values extends HostNetInitFormikExtension>(
                 <MonoText>{`${ip}/${subnetMask}`}</MonoText>
               </Grid>
             </Grid>
+          </Grid>
+        );
+      })}
+      {noIface.map((netId, index, array) => {
+        const { sequence, type } = values.networkInit.networks[netId];
+
+        const key = getNetKey(netId);
+        const name = getNetName(type, sequence);
+        const short = getNetShort(type, sequence);
+
+        const mb = index === array.length - 1 ? '1.4em' : '.2em';
+
+        return (
+          <Grid key={key} item mb={mb} width="100%">
+            <MessageBox>
+              No interface(s) set for {name} (
+              <InlineMonoText inheritColour>{short}</InlineMonoText>), it will
+              be discarded.
+            </MessageBox>
           </Grid>
         );
       })}
