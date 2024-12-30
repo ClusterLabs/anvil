a64d998a2c115fa369a57fc1b8250cc9a067e9d3
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2024-12-29 22:50:06 -0500

    fix(striker-ui): include offline as anvil status

--- striker-ui/components/Anvils/Anvil.tsx
+++ striker-ui/components/Anvils/Anvil.tsx
@@ -5,7 +5,7 @@ const Anvil = ({ anvil }: { anvil: AnvilListItem }): JSX.Element => (
   <>
     <BodyText text={anvil.anvil_name} />
     <BodyText
-      text={anvilState.get(anvil.anvilStatus.system) || 'State unavailable'}
+      text={anvilState.get(anvil.anvilStatus.system) ?? 'State unavailable'}
     />
   </>
 );
