86fca216b9d483a13b6c94720c7ea0ce96eff6ec
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-01-13 18:35:00 -0500

    fix(striker-ui): remove bad key message after clear in test access form

--- striker-ui/components/ManageHost/TestAccessForm.tsx
+++ striker-ui/components/ManageHost/TestAccessForm.tsx
@@ -88,6 +88,7 @@ const TestAccessForm: FC<TestAccessFormProps> = (props) => {
                               ),
                             });
 
+                            setApiMessage();
                             setMoreActions([]);
                           })
                           .catch((error) => {
