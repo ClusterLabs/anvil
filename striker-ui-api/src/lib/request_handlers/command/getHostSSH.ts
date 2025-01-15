e74fa4458abddd9d4a40364470374b18e1187547
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-01-14 19:55:30 -0500

    fix(striker-ui-api): include bad keys for same host in inquire-host

--- striker-ui-api/src/lib/request_handlers/command/getHostSSH.ts
+++ striker-ui-api/src/lib/request_handlers/command/getHostSSH.ts
@@ -39,12 +39,62 @@ export const getHostSSH: RequestHandler<
   let badKeys: string[];
 
   try {
-    const rows = await query<[string][]>(`
-      SELECT DISTINCT(
-        SUBSTRING(state_note, 'key=(.*)')
-      ) as bad_key
+    // Try matching the target with an IP to get the host UUID.
+    const sqlGetUuidFromIp = `
+      SELECT ip_address_host_uuid
+      FROM ip_addresses
+      WHERE ip_address_address = '${target}'
+      LIMIT 1`;
+
+    // Try matching the target with a host name to get the host UUID.
+    const sqlGetUuidFromShort = `
+      SELECT host_uuid
+      FROM hosts
+      WHERE host_name LIKE CONCAT(
+        SUBSTRING('${target}', '^[^.]*'),
+        '%'
+      )
+      LIMIT 1`;
+
+    // Since the target can only be **either** a name or IP, 1/2 query will
+    // return NULL. When there's a match, prioritize the one that isn't NULL.
+    const sqlGetUuid = `
+      SELECT
+        COALESCE(a1.host_uuid, a2.ip_address_host_uuid) AS host_uuid
+      FROM (${sqlGetUuidFromShort}) AS a1
+      FULL JOIN (${sqlGetUuidFromIp}) AS a2
+        ON TRUE`;
+
+    // Filter and format the state values before doing the final match.
+    const sqlGetStates = `
+      SELECT
+        state_uuid,
+        SUBSTRING(state_name, '${HOST_KEY_CHANGED_PREFIX}(.*)') AS target,
+        SUBSTRING(state_note, 'key=(.*)') AS key
       FROM states
-      WHERE state_name = '${HOST_KEY_CHANGED_PREFIX}${target}';`);
+      WHERE state_name LIKE '${HOST_KEY_CHANGED_PREFIX}%'`;
+
+    // Get all IPs and the host name, then match them to the state records to
+    // find all keys related to the target.
+    const sqlGetKeys = `
+      SELECT DISTINCT(d.key)
+      FROM (${sqlGetUuid}) AS a
+      LEFT JOIN ip_addresses AS b
+        ON a.host_uuid = b.ip_address_host_uuid
+      LEFT JOIN hosts AS c
+        ON a.host_uuid = c.host_uuid
+      JOIN (${sqlGetStates}) AS d
+        ON d.target LIKE ANY (
+          ARRAY [
+            b.ip_address_address,
+            CONCAT(
+              SUBSTRING(c.host_name, '^[^.]*'),
+              '%'
+            )
+          ]
+        )`;
+
+    const rows = await query<[string][]>(`${sqlGetKeys};`);
 
     badKeys = rows.map(([badKey]) => badKey);
   } catch (error) {
