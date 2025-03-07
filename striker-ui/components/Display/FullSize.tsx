15f3109dc10ca22bc35f634a832e41ea7fdf1b3e
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-03-07 17:35:00 -0500

    fix(striker-ui): reduce reconnect timeout, always show toolbar in full screen

--- striker-ui/components/Display/FullSize.tsx
+++ striker-ui/components/Display/FullSize.tsx
@@ -45,7 +45,7 @@ const StyledDiv = styled('div')(() => ({
 const VncDisplay = dynamic(() => import('./VncDisplay'), { ssr: false });
 
 // Unit: seconds
-const DEFAULT_VNC_RECONNECT_TIMER_START = 10;
+const DEFAULT_VNC_RECONNECT_TIMER_START = 3;
 
 const MAP_TO_WSCODE_MSG: Record<number, string> = {
   1000: 'in-use by another process?',
@@ -296,23 +296,21 @@ const FullSize = <Node extends NodeMinimum, Server extends ServerMinimum>(
   );
 
   const vncToolbarElement = useMemo(
-    () =>
-      showScreen && (
-        <>
-          {fullscreenElement}
-          {keyboardMenuElement}
-          <ServerMenu node={node} server={server} />
-          {returnHomeElement}
-          {vncDisconnectElement}
-        </>
-      ),
+    () => (
+      <>
+        {fullscreenElement}
+        {keyboardMenuElement}
+        <ServerMenu node={node} server={server} />
+        {returnHomeElement}
+        {vncDisconnectElement}
+      </>
+    ),
     [
       fullscreenElement,
       keyboardMenuElement,
       node,
       returnHomeElement,
       server,
-      showScreen,
       vncDisconnectElement,
     ],
   );
