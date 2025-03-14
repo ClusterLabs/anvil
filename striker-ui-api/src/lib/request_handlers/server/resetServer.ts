f2bd18e4d61aa0774ed2eb94dc156274fd84ce6b
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-03-13 20:22:36 -0400

    fix(striker-ui-api): correct job data variable in reset server

--- striker-ui-api/src/lib/request_handlers/server/resetServer.ts
+++ striker-ui-api/src/lib/request_handlers/server/resetServer.ts
@@ -6,7 +6,7 @@ export const resetServer = buildServerUpdateHandler(
   async (request, server, sbin) => ({
     job_command: sbin['anvil-shutdown-server'].self,
     job_data: buildJobDataFromObject({
-      server_uuid: server.uuid,
+      'server-uuid': server.uuid,
       task: 'reset',
     }),
     job_description: `job_0339`,
