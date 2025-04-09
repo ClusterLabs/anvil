6d5cd2a5fe996de862417eff6b78455ee0ca51dc
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-04-09 15:39:05 -0400

    fix(striker-ui): correct paasing types in use fetch hook

--- striker-ui/hooks/useFetch.tsx
+++ striker-ui/hooks/useFetch.tsx
@@ -1,32 +1,49 @@
+import { AxiosError, AxiosResponse } from 'axios';
 import { useMemo } from 'react';
 import useSWR, { BareFetcher, KeyedMutator, SWRConfiguration } from 'swr';
 
 import api from '../lib/api';
 
-type FetchHookResponse<D, E extends Error = Error> = {
-  data?: D;
-  error?: E;
-  mutate: KeyedMutator<D>;
+type FetchHookResponse<
+  ResData,
+  AltResData = ResData,
+  ReqData = unknown,
+  Err extends Error = AxiosError<ReqData, ResData>,
+> = {
+  altData?: AltResData;
+  data?: ResData;
+  error?: Err;
+  mutate: KeyedMutator<ResData>;
   loading: boolean;
   validating: boolean;
 };
 
-const useFetch = <Data, Alt = Data>(
+const useFetch = <
+  ResData,
+  AltResData = ResData,
+  ReqData = unknown,
+  Err extends Error = AxiosError<ReqData, ResData>,
+>(
   url: string,
-  options: SWRConfiguration<Data> & {
-    fetcher?: BareFetcher<Data>;
-    mod?: (data: Data) => Alt;
+  options: SWRConfiguration<ResData, Err, BareFetcher<ResData>> & {
+    mod?: (data: ResData) => AltResData;
     periodic?: boolean;
     timeout?: number;
   } = {},
-): FetchHookResponse<Data> & { altData?: Alt } => {
+): FetchHookResponse<ResData, AltResData, ReqData, Err> => {
   const {
     timeout = 5000,
     mod,
     periodic,
     // Depends on the above
     fetcher = async (l) => {
-      const response = await api.get(l, { timeout });
+      const response = await api.get<
+        ResData,
+        AxiosResponse<ResData, ReqData>,
+        ReqData
+      >(l, {
+        timeout,
+      });
 
       return response.data;
     },
@@ -44,12 +61,12 @@ const useFetch = <Data, Alt = Data>(
     error,
     isValidating: validating,
     mutate,
-  } = useSWR<Data>(url, fetcher, {
+  } = useSWR<ResData, Err>(url, fetcher, {
     refreshInterval,
     ...config,
   });
 
-  const altData = useMemo<Alt | undefined>(
+  const altData = useMemo<AltResData | undefined>(
     () => mod && data && mod(data),
     [data, mod],
   );
