c9e4056feac3ce689a673eccc5157c87b55f79cd
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-04-11 23:58:19 -0400

    fix(striker-ui): revise node's storage group display

--- striker-ui/components/SharedStorage/StorageGroup.tsx
+++ striker-ui/components/SharedStorage/StorageGroup.tsx
@@ -1,63 +1,102 @@
-import { Box, styled } from '@mui/material';
+import { Grid } from '@mui/material';
+import { dSizeStr } from 'format-data-size';
 import { useMemo } from 'react';
 
-import { AllocationBar } from '../Bars';
-import { toBinaryByte } from '../../lib/format_data_size_wrappers';
-import { BodyText } from '../Text';
-
-const PREFIX = 'StorageGroup';
-
-const classes = {
-  fs: `${PREFIX}-fs`,
-  bar: `${PREFIX}-bar`,
-  decoratorBox: `${PREFIX}-decoratorBox`,
-};
-
-const StyledDiv = styled('div')(() => ({
-  [`& .${classes.fs}`]: {
-    paddingLeft: '.7em',
-    paddingRight: '.7em',
-  },
-
-  [`& .${classes.bar}`]: {
-    paddingLeft: '.7em',
-    paddingRight: '.7em',
-  },
-
-  [`& .${classes.decoratorBox}`]: {
-    paddingRight: '.3em',
-  },
-}));
+import { StorageBar } from '../Bars';
+import { InnerPanel, InnerPanelBody, InnerPanelHeader } from '../Panels';
+import { BodyText, InlineMonoText } from '../Text';
 
 const StorageGroup: React.FC<StorageGroupProps> = (props) => {
-  const { storageGroup: group } = props;
+  const { storageGroup } = props;
 
-  const { free: nFree, size: nTotal, used: nUsed } = group;
+  const { members, name } = storageGroup;
 
-  const percentAllocated = useMemo(
-    () => Number((nUsed * BigInt(100)) / nTotal),
-    [nUsed, nTotal],
+  const sgFree = useMemo<string>(
+    () => dSizeStr(storageGroup.free, { toUnit: 'ibyte' }) ?? 'none',
+    [storageGroup.free],
+  );
+
+  const sgSize = useMemo<string>(
+    () => dSizeStr(storageGroup.size, { toUnit: 'ibyte' }) ?? 'none',
+    [storageGroup.size],
+  );
+
+  const sgUsed = useMemo<string>(
+    () => dSizeStr(storageGroup.used, { toUnit: 'ibyte' }) ?? 'none',
+    [storageGroup.used],
   );
 
   return (
-    <StyledDiv>
-      <Box display="flex" width="100%" className={classes.fs}>
-        <Box flexGrow={1}>
-          <BodyText text={`Used: ${toBinaryByte(nTotal - nFree)}`} />
-        </Box>
-        <Box>
-          <BodyText text={`Free: ${toBinaryByte(nFree)}`} />
-        </Box>
-      </Box>
-      <Box display="flex" width="100%" className={classes.bar}>
-        <Box flexGrow={1}>
-          <AllocationBar allocated={percentAllocated} />
-        </Box>
-      </Box>
-      <Box display="flex" justifyContent="center" width="100%">
-        <BodyText text={`Total Storage: ${toBinaryByte(nTotal)}`} />
-      </Box>
-    </StyledDiv>
+    <InnerPanel>
+      <InnerPanelHeader>
+        <BodyText>{name}</BodyText>
+      </InnerPanelHeader>
+      <InnerPanelBody>
+        <Grid container rowSpacing="1em">
+          <Grid item width="100%">
+            <Grid container>
+              <Grid item width="100%">
+                <Grid container>
+                  <Grid item>
+                    <BodyText>
+                      Used: <InlineMonoText>{sgUsed}</InlineMonoText>
+                    </BodyText>
+                  </Grid>
+                  <Grid item xs />
+                  <Grid item textAlign="right">
+                    <BodyText>
+                      Free: <InlineMonoText>{sgFree}</InlineMonoText>
+                    </BodyText>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item width="100%">
+                <StorageBar storageGroup={storageGroup} />
+              </Grid>
+              <Grid item width="100%">
+                <BodyText>
+                  Total: <InlineMonoText>{sgSize}</InlineMonoText>
+                </BodyText>
+              </Grid>
+            </Grid>
+          </Grid>
+          {Object.values(members).map((member) => {
+            const { volumeGroup } = member;
+
+            const vgFree =
+              dSizeStr(volumeGroup.free, { toUnit: 'ibyte' }) ?? 'none';
+
+            const vgSize =
+              dSizeStr(volumeGroup.size, { toUnit: 'ibyte' }) ?? 'none';
+
+            return (
+              <Grid item key={member.uuid} width="100%">
+                <Grid container>
+                  <Grid item width="100%">
+                    <BodyText>{volumeGroup.name}</BodyText>
+                  </Grid>
+                  <Grid item xs>
+                    <BodyText variant="caption">
+                      {volumeGroup.host.short}
+                    </BodyText>
+                  </Grid>
+                  <Grid item xs>
+                    <BodyText mb="-.3em" variant="caption">
+                      Free
+                      <InlineMonoText>{vgFree}</InlineMonoText>/
+                      <InlineMonoText>{vgSize}</InlineMonoText>
+                    </BodyText>
+                  </Grid>
+                  <Grid item width="100%">
+                    <StorageBar volumeGroup={volumeGroup} />
+                  </Grid>
+                </Grid>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </InnerPanelBody>
+    </InnerPanel>
   );
 };
 
