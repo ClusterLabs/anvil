cf4e5b14ec4f5885dad850392a68eb17cd65106f
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-04-16 13:53:54 -0400

    fix(striker-ui-api): add yup lvm uuid schema

--- striker-ui-api/src/lib/yupCommons.ts
+++ striker-ui-api/src/lib/yupCommons.ts
@@ -1,21 +1,6 @@
 import * as yup from 'yup';
 
-import { REP_IPV4, REP_MAC, REP_UUID } from './consts';
-
-export const yupLaxMac = () =>
-  yup.string().matches(REP_MAC, {
-    message: '${path} must be a valid MAC address',
-  });
-
-export const yupLaxUuid = () =>
-  yup.string().matches(REP_UUID, {
-    message: '${path} must be a valid UUID',
-  });
-
-export const yupIpv4 = () =>
-  yup.string().matches(REP_IPV4, {
-    message: '${path} must be a valid IPv4 address',
-  });
+import { REP_IPV4, REP_LVM_UUID, REP_MAC, REP_UUID } from './consts';
 
 export const yupDynamicObject = <S extends yup.Schema>(
   input: yup.AnyObject,
@@ -28,3 +13,23 @@ export const yupDynamicObject = <S extends yup.Schema>(
     }),
     {},
   );
+
+export const yupIpv4 = () =>
+  yup.string().matches(REP_IPV4, {
+    message: '${path} must be a valid IPv4 address',
+  });
+
+export const yupLaxMac = () =>
+  yup.string().matches(REP_MAC, {
+    message: '${path} must be a valid MAC address',
+  });
+
+export const yupLaxUuid = () =>
+  yup.string().matches(REP_UUID, {
+    message: '${path} must be a valid UUID',
+  });
+
+export const yupLvmUuid = () =>
+  yup.string().matches(REP_LVM_UUID, {
+    message: '${path} must be a valid LVM internal UUID',
+  });
