45e4ee709d5b2b9dcd971ad443ba5815c8ba2a93
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-05-01 00:28:12 -0400

    fix(striker-ui-api): add helper to set value nested in object

---
+++ striker-ui-api/src/lib/chain.ts
@@ -0,0 +1,21 @@
+export const setChain = <T extends boolean | number | string>(
+  chain: string[],
+  value: T | Tree<T>,
+  parent: Tree<T> = {},
+): T | Tree<T> => {
+  const { 0: key, length } = chain;
+
+  if (!key) {
+    return parent;
+  }
+
+  const { [key]: existing } = parent;
+
+  if (length > 1 && typeof existing === 'object' && existing !== null) {
+    parent[key] = setChain(chain.slice(1), value, existing);
+  } else {
+    parent[key] = value;
+  }
+
+  return parent;
+};
