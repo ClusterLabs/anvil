5eaf9c1278aa1ad2b5bdeca386093cc5ae2c0a0c
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-06-12 23:07:48 -0400

    fix(striker-ui): add cookies provider to app root

--- striker-ui/pages/_app.tsx
+++ striker-ui/pages/_app.tsx
@@ -1,6 +1,7 @@
-import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material';
 import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider } from '@mui/material';
+import { AppProps } from 'next/app';
+import { CookiesProvider } from 'react-cookie';
 
 import createEmotionCache from '../lib/create_emotion_cache/createEmotionCache';
 import theme from '../theme';
@@ -14,17 +15,29 @@ interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
-const App = ({
-  Component,
-  emotionCache = clientSideEmotionCache,
-  pageProps,
-}: MyAppProps): React.ReactElement => {
+/**
+ * Wraps the actual page component (`Component`).
+ *
+ * It allows react hooks to access app-wide providers that are included in the
+ * `App` component. For example, the `useSessionExpiryCheck()` hook requires the
+ * `useCookies()` hook from the `react-cookie` package, which requires the
+ * `<CookiesProvider />`.
+ */
+const PageWrapper: React.FC<MyAppProps> = ({ Component, pageProps }) => {
   useSessionExpiryCheck();
+
+  return <Component {...pageProps} />;
+};
+
+const App: React.FC<MyAppProps> = (props) => {
+  const { emotionCache = clientSideEmotionCache } = props;
 
   return (
     <CacheProvider value={emotionCache}>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <CookiesProvider>
+          <PageWrapper {...props} />
+        </CookiesProvider>
       </ThemeProvider>
     </CacheProvider>
   );
