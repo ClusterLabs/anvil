dfa00e0e146622498c57329a87039661ef298bc9
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-06-17 00:55:47 -0400

    build(striker-ui): allow async in web in build config to resolve warning

--- striker-ui/next.config.mjs
+++ striker-ui/next.config.mjs
@@ -8,12 +8,20 @@ export default () => {
     pageExtensions: ['ts', 'tsx'],
     poweredByHeader: false,
     reactStrictMode: true,
-    webpack: (config) => {
+    webpack: (config, { isServer }) => {
       config.experiments = {
         ...config.experiments,
         // Make builds work with novnc >= v1.6.0.
         topLevelAwait: true,
       };
+
+      if (!isServer) {
+        config.output.environment = {
+          ...config.output.environment,
+          // Stop non-async environment warning.
+          asyncFunction: true,
+        };
+      }
 
       return config;
     },
