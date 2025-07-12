7503f36de7bf7b8c5e640b719fe084ce3adee34c
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-07-11 21:19:49 -0400

    fix(striker-ui-api): add delete and rename states to server list

--- striker-ui-api/src/lib/request_handlers/server/getServer.ts
+++ striker-ui-api/src/lib/request_handlers/server/getServer.ts
@@ -1,10 +1,8 @@
-import { DELETED } from '../../consts';
-
 import buildGetRequestHandler from '../buildGetRequestHandler';
 import { buildQueryResultReducer } from '../../buildQueryResultModifier';
-import { getShortHostName } from '../../disassembleHostName';
 import join from '../../join';
 import { sanitize } from '../../sanitize';
+import { sqlHosts, sqlServers } from '../../sqls';
 
 export const getServer = buildGetRequestHandler((request, hooks) => {
   const { anvilUUIDs } = request.query;
@@ -33,13 +31,16 @@ export const getServer = buildGetRequestHandler((request, hooks) => {
       a.anvil_description,
       a.host_uuid,
       a.host_name,
+      a.host_short_name,
       a.host_type,
       d.job_uuid,
       d.job_progress,
       d.job_on_peer,
       d.server_name,
+      d.server_state_from_job,
       d.host_uuid,
       d.host_name,
+      d.host_short_name,
       d.host_type,
       d.anvil_uuid,
       d.anvil_name,
@@ -54,13 +55,14 @@ export const getServer = buildGetRequestHandler((request, hooks) => {
           a2.anvil_description,
           a3.host_uuid,
           a3.host_name,
+          a3.host_short_name,
           a3.host_type
-        FROM servers AS a1
+        FROM (${sqlServers()}) AS a1
         JOIN anvils AS a2
           ON a2.anvil_uuid = a1.server_anvil_uuid
-        LEFT JOIN hosts AS a3
+        LEFT JOIN (${sqlHosts()}) AS a3
           ON a3.host_uuid = a1.server_host_uuid
-        WHERE a1.server_state != '${DELETED}'
+        WHERE TRUE
           ${condServerAnvil}
         ORDER BY a1.server_name ASC
       ) AS a
@@ -74,22 +76,40 @@ export const getServer = buildGetRequestHandler((request, hooks) => {
             ELSE 0
           END AS job_on_peer,
           SUBSTRING(d1.job_data, 'server_name=([^\\n]*)') AS server_name,
+          CASE
+            WHEN d1.job_command LIKE '%delete-server%'
+              THEN 'deleting'
+            WHEN d1.job_command LIKE '%provision-server%'
+              THEN 'provisioning'
+            WHEN d1.job_command LIKE '%rename-server%'
+              THEN 'renaming'
+            ELSE NULL
+          END as server_state_from_job,
           d2.host_uuid,
           d2.host_name,
+          d2.host_short_name,
           d2.host_type,
           d3.anvil_uuid,
           d3.anvil_name,
           d3.anvil_description
         FROM jobs AS d1
-        JOIN hosts AS d2
+        JOIN (${sqlHosts()}) AS d2
           ON d2.host_uuid = d1.job_host_uuid
         JOIN anvils AS d3
           ON d2.host_uuid IN (
             d3.anvil_node1_host_uuid,
             d3.anvil_node2_host_uuid
           )
-        WHERE d1.job_command LIKE '%anvil-provision-server%'
-          AND d1.modified_date > current_timestamp - interval '5 minutes'
+        WHERE
+            d1.job_command LIKE ANY (
+              ARRAY[
+                '%delete-server%',
+                '%provision-server%',
+                '%rename-server%'
+              ]
+            )
+          AND
+            d1.modified_date > current_timestamp - interval '5 minutes'
           ${condJobAnvil}
         ORDER BY
           server_name ASC,
@@ -109,31 +129,34 @@ export const getServer = buildGetRequestHandler((request, hooks) => {
         anvilDescription,
         hostUuid,
         hostName,
+        hostShortName,
         hostType,
         jobUuid,
         jobProgress,
         jobOnPeer,
         jobServerName,
+        jobServerState,
         jobHostUuid,
         jobHostName,
+        jobHostShortName,
         jobHostType,
         jobAnvilUuid,
         jobAnvilName,
         jobAnvilDescription,
       ] = row;
 
+      let host: ServerOverviewHost | undefined;
+
+      if (hostUuid) {
+        host = {
+          name: hostName,
+          short: hostShortName,
+          type: hostType,
+          uuid: hostUuid,
+        };
+      }
+
       if (serverUuid) {
-        let host: ServerOverviewHost | undefined;
-
-        if (hostUuid) {
-          host = {
-            name: hostName,
-            short: getShortHostName(hostName),
-            type: hostType,
-            uuid: hostUuid,
-          };
-        }
-
         previous[serverUuid] = {
           anvil: {
             description: anvilDescription,
@@ -145,41 +168,45 @@ export const getServer = buildGetRequestHandler((request, hooks) => {
           state: serverState,
           uuid: serverUuid,
         };
-      } else if (jobUuid) {
-        if (!previous[jobServerName]) {
-          previous[jobServerName] = {
-            anvil: {
-              description: jobAnvilDescription,
-              name: jobAnvilName,
-              uuid: jobAnvilUuid,
-            },
-            host: {
-              name: jobHostName,
-              short: getShortHostName(jobHostName),
-              type: jobHostType,
-              uuid: jobHostUuid,
-            },
-            name: jobServerName,
-            state: 'provisioning',
-            uuid: jobUuid,
-          };
-        }
+      }
+
+      if (jobUuid) {
+        host = {
+          name: jobHostName,
+          short: jobHostShortName,
+          type: jobHostType,
+          uuid: jobHostUuid,
+        };
+
+        previous[jobServerName] = previous[jobServerName] ?? {
+          anvil: {
+            description: jobAnvilDescription,
+            name: jobAnvilName,
+            uuid: jobAnvilUuid,
+          },
+          host,
+          name: jobServerName,
+          state: '',
+          uuid: jobUuid,
+        };
 
         const { [jobServerName]: server } = previous;
 
-        if (!server.jobs) {
-          server.jobs = {};
+        const peer = Number(jobOnPeer) === 1;
+
+        const progress = Number(jobProgress);
+
+        if (progress < 100 && jobServerState) {
+          // Update server state based on the running job
+          server.state = jobServerState;
         }
 
+        server.jobs = server.jobs ?? {};
+
         server.jobs[jobUuid] = {
-          host: {
-            name: jobHostName,
-            short: getShortHostName(jobHostName),
-            type: jobHostType,
-            uuid: jobHostUuid,
-          },
-          peer: Number(jobOnPeer) === 1,
-          progress: Number(jobProgress),
+          host,
+          peer,
+          progress,
           uuid: jobUuid,
         };
       }
