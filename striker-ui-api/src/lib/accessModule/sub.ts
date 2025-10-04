33e9df27ab7d4c37345c5e467e2ecc2ef7a598de
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2025-10-04 03:57:24 -0400

    fix(striker-ui-api): properly escape nested quotes in sub of access module

--- striker-ui-api/src/lib/accessModule/sub.ts
+++ striker-ui-api/src/lib/accessModule/sub.ts
@@ -1,5 +1,48 @@
 import { access } from './instance';
 
+// Un-does `x = 2 ^ n` without logs
+const un2n = (value: number): number => {
+  let x = value;
+  let n = 0;
+
+  while (x > 0) {
+    x >>= 1;
+    n += 1;
+  }
+
+  return n;
+};
+
+const quote = (value: string): string => {
+  const matches = value.match(/\\*/g);
+
+  if (!matches) {
+    return value;
+  }
+
+  const result = matches.reduce<string>((previous, before) => {
+    const quotes = before.match(/\\/g) ?? [];
+
+    // The number of quotes in each "layer" can be calculated with `2 ^ n - 1`
+
+    // FROM SANDBOX:
+    // ivt = (value) => { let x = value; let n = -1; while (x > 0) { x >>= 1; n += 1; } return n; }
+    // sample.match(/\\*"/g).reduce((previous, pattern) => previous.replace(new RegExp(`(?<!)${pattern}`, 'g'), '"'.padStart(Math.pow(2, ivt((pattern.match(/\\/g) ?? []).length + 1) + 1) - 1, '\\')), `${sample}`)
+
+    const exponent = un2n(quotes.length + 1);
+
+    const count = Math.pow(2, exponent) - 1;
+
+    const after = '"'.padStart(count, '\\');
+
+    const pattern = new RegExp(`(?<!)${before}`, 'g');
+
+    return previous.replace(pattern, after);
+  }, value);
+
+  return `"${result}"`;
+};
+
 export const opSub = (
   subroutine: string,
   {
@@ -22,7 +65,7 @@ export const opSub = (
       result = String(p);
     }
 
-    return `"${result.replaceAll('"', '\\"')}"`;
+    return quote(result);
   });
 
   return `x ${chain} ${subParams.join(' ')}`;
